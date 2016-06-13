bd1efbb606c4afedfd305c9699b1d5acc7f06959
Redth <jondick@gmail.com>
2016-06-13 13:18:29 -0400

    [card.io] add missing XCode project from externals

---
+++ XPlat/CardIO/iOS/externals/CardIOSharp/CardIOSharp/CardIOKeeper.m
@@ -0,0 +1,13 @@
+////
+////  CardIOKeeper.m
+////  CardIOSharp
+////
+////  Created by Bill Holmes on 1/18/16.
+////  Copyright © 2016 Xamarin. All rights reserved.
+////
+//
+//#import "CardIOKeeper.h"
+//
+//@implementation CardIOKeeper
+//
+//@end
---
+++ XPlat/CardIO/iOS/externals/CardIOSharp/CardIOSharp/CardIOSharp.h
@@ -0,0 +1,19 @@
+//
+//  TestFramework.h
+//  TestFramework
+//
+//  Created by Jonathan Dick on 2016-01-19.
+//  Copyright © 2016 Xamarin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestFramework.
+FOUNDATION_EXPORT double CardIOSharpVersionNumber;
+
+//! Project version string for TestFramework.
+FOUNDATION_EXPORT const unsigned char CardIOSharpVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFramework/PublicHeader.h>
+//#import <CardIOSharp/CardIOKeeper.h>
+
