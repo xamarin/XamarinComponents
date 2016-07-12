f3fb5ebe72498c78425fbeac3d22bd19741177dc
Redth <jondick@gmail.com>
2016-07-12 13:12:36 -0400

    [mapbox] fixed manifest declarations for binding

--- XPlat/Mapbox/Android/source/MapboxSDK.Android/Additions/Manifest.cs
+++ XPlat/Mapbox/Android/source/MapboxSDK.Android/Additions/Manifest.cs
@@ -4,16 +4,26 @@ using Android.App;
 using Android.OS;
 using Android.Content;
 
+// Mapzen Lost
+[assembly: UsesPermission (Android.Manifest.Permission.AccessCoarseLocation)]
+[assembly: UsesPermission (Android.Manifest.Permission.AccessFineLocation)]
+[assembly: UsesFeature ("android.hardware.sensor.accelerometer", Required = false)]
+[assembly: UsesFeature ("android.hardware.location")]
+[assembly: UsesFeature ("android.hardware.location.gps")]
+[assembly: UsesFeature ("android.hardware.telephony", Required = false)]
+[assembly: UsesFeature ("android.hardware.wifi")]
+
+// Mapbox
+[assembly: UsesFeature (GLESVersion=0x00020000, Required=true)]
+[assembly: UsesPermission (Android.Manifest.Permission.Internet)]
+[assembly: UsesPermission (Android.Manifest.Permission.AccessNetworkState)]
+[assembly: UsesPermission (Android.Manifest.Permission.AccessWifiState)]
+
 namespace Mapbox.Telemetry
 {
-    [Register ("com/mapbox/mapboxsdk/telemetry/TelemetryService", DoNotGenerateAcw=true)]
-    [Service (Name="com.mapbox.mapboxsdk.telemetry.TelemetryService")]
-    internal class _InternalTelemetryService : Service
+    [Register ("com/mapbox/mapboxsdk/telemetry/TelemetryService", DoNotGenerateAcw = true)]
+    [Service (Name = "com.mapbox.mapboxsdk.telemetry.TelemetryService")]
+    partial class TelemetryService
     {
-        public override IBinder OnBind (Intent intent)
-        {
-            return default (IBinder);
-        }
     }
 }
-
