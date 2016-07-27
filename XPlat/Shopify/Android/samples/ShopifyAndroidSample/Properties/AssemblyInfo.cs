053972470233e63377e6d3606f3272fa309dba21
Matthew Leibowitz <mattleibow@live.com>
2016-07-27 23:38:23 +0200

    Added licensing

--- XPlat/Shopify/Android/samples/ShopifyAndroidSample/Properties/AssemblyInfo.cs
+++ XPlat/Shopify/Android/samples/ShopifyAndroidSample/Properties/AssemblyInfo.cs
@@ -34,7 +34,7 @@ using Android.App;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("(c) Matthew Leibowitz")]
+[assembly: AssemblyCopyright("Copyright (c) 2016 Xamarin, Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -42,7 +42,7 @@ using Android.App;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
