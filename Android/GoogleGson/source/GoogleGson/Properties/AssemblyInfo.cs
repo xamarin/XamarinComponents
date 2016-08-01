09052fc77ae29d7e254d0d90d82555d8814e1395
Matthew Leibowitz <mattleibow@live.com>
2016-08-01 23:35:22 +0200

    [GoogleGson] Updated the library to v2.7

--- Android/GoogleGson/source/GoogleGson/Properties/AssemblyInfo.cs
+++ Android/GoogleGson/source/GoogleGson/Properties/AssemblyInfo.cs
@@ -18,8 +18,8 @@ using Android.App;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("2.6.2.0")]
-[assembly: AssemblyFileVersion ("2.6.2.0")]
+[assembly: AssemblyVersion ("2.7.0.0")]
+[assembly: AssemblyFileVersion ("2.7.0.0")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
