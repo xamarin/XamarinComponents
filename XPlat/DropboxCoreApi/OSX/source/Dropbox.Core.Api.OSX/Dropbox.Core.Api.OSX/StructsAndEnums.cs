29247c15d98f0bac5e6dc188e550307e4107247d
adlair <adlair.cerecedo@xamarin.com>
2016-10-28 03:23:29 -0500

    OSX version added.

---
+++ XPlat/DropboxCoreApi/OSX/source/Dropbox.Core.Api.OSX/Dropbox.Core.Api.OSX/StructsAndEnums.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Dropbox.CoreApi.OSX
+{
+	public enum ErrorCode : uint
+	{
+		None = 0,
+		GenericError = 1000,
+		FileNotFound,
+		InsufficientDiskSpace,
+		IllegalFileType,
+		InvalidResponse
+	}
+
+	public enum JsonError : uint
+	{
+		Unsupported = 1,
+		Parsenum,
+		Parse,
+		Fragment,
+		Ctrl,
+		Unicode,
+		Depth,
+		Escape,
+		Trailcomma,
+		Trailgarbage,
+		Eof,
+		Input
+	}
+
+
+}
+
+namespace Dropbox.CoreApi.OSX.MPOAuth
+{
+	public enum SignatureScheme : uint
+	{
+		PlainText,
+		Hmacsha1,
+		Rsasha1
+	}
+
+	public enum AuthenticationState : uint
+	{
+		Unauthenticated = 0,
+		Authenticating = 1,
+		Authenticated = 2
+	}
+}
