ef909b05d5fa27a004dcb9847b11089dc7bbadd8
Redth <jondick@gmail.com>
2016-12-20 12:37:36 -0500

    [android-things] Made sample a bit more interesting
    Now lists the GPIO’s from the Things SDK

--- Android/AndroidThings/samples/SampleThing/MainActivity.cs
+++ Android/AndroidThings/samples/SampleThing/MainActivity.cs
@@ -6,12 +6,15 @@ using Android.Things.Pio;
 
 namespace SampleThing
 {
-    [Activity(Label = "Sample Thing")]
+    [Activity(Label = "Xamarin Android Thing")]
     [IntentFilter (new[] { Intent.ActionMain }, Categories = new[] { Intent.CategoryLauncher })]
     [IntentFilter(new[] { Intent.ActionMain }, Categories = new[] { "android.intent.category.IOT_LAUNCHER" })]
     public class MainActivity : Activity
     {
         const string TAG = "SAMPLE_THING";
+
+        ListView listView;
+        ArrayAdapter<string> adapter;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -20,9 +23,17 @@ namespace SampleThing
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
+            listView = FindViewById<ListView>(Resource.Id.listView1);
+
             var peripheralMgr = new PeripheralManagerService();
-            foreach (var gpio in peripheralMgr.GpioList)
+
+            var gpios = peripheralMgr.GpioList;
+            foreach (var gpio in gpios)
                 Android.Util.Log.Debug(TAG, "Found Peripheral: {0}", gpio);
+
+            adapter = new ArrayAdapter<string>(this, global::Android.Resource.Layout.SimpleListItem1, gpios);
+
+            listView.Adapter = adapter;
         }
     }
 }
