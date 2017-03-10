c9baa5fda3654c1878cdd4dfa08359aab30d07b0
Matthew Leibowitz <mattleibow@live.com>
2017-03-10 16:04:23 +0200

    [TIP] Added members so that the user can log via TIP (aka TIPLog)

--- iOS/TwitterImagePipeline/source/TwitterImagePipeline/Additions.cs
+++ iOS/TwitterImagePipeline/source/TwitterImagePipeline/Additions.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using CoreGraphics;
 using Foundation;
+using ObjCRuntime;
 using UIKit;
 
 namespace TwitterImagePipeline
@@ -14,6 +16,38 @@ namespace TwitterImagePipeline
 			This.Inspect(results => tcs.SetResult(results));
 			return tcs.Task;
 		}
+	}
+
+	partial class TIPLogger
+	{
+		private static Selector tip_canLogWithLevel = new Selector("tip_canLogWithLevel:");
+
+		private static void LogInternal(TIPLogLevel level, string format, params object[] args)
+		{
+			var logger = TIPGlobalConfiguration.SharedInstance.Logger;
+			var nsLogger = logger as NSObject;
+
+			if (logger != null && (nsLogger == null || !nsLogger.RespondsToSelector(tip_canLogWithLevel) || logger.CanLog(level)))
+			{
+				var stacktrace = new StackTrace(true);
+				var frame = stacktrace.FrameCount >= 2 ? stacktrace.GetFrame(2) : null;
+
+				var file = frame?.GetFileName();
+				var method = frame?.GetMethod();
+				var function = method == null ? null : method.DeclaringType.FullName + ": " + method.ToString();
+				var line = frame?.GetFileLineNumber();
+
+				var message = string.Format(format, args);
+
+				logger.Log(level, file ?? string.Empty, function ?? string.Empty, line ?? 0, message, IntPtr.Zero);
+			}
+		}
+
+		public static void Log(TIPLogLevel level, string format, params object[] args) => LogInternal(level, format, args);
+		public static void LogError(string format, params object[] args) => LogInternal(TIPLogLevel.Error, format, args);
+		public static void LogWarning(string format, params object[] args) => LogInternal(TIPLogLevel.Warning, format, args);
+		public static void LogInformation(string format, params object[] args) => LogInternal(TIPLogLevel.Information, format, args);
+		public static void LogDebug(string format, params object[] args) => LogInternal(TIPLogLevel.Debug, format, args);
 	}
 
 	public class TIPSimpleImageFetchRequest : NSObject, ITIPImageFetchRequest
