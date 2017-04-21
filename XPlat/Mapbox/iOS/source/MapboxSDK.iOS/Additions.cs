b09b68884f0499246acff96df9fa5d07eb1fd171
Davide Mendolia <davide@gokarumi.com>
2017-04-21 19:06:49 +0200

    Add mapping to CLLocationCoordinate2D array

--- XPlat/Mapbox/iOS/source/MapboxSDK.iOS/Additions.cs
+++ XPlat/Mapbox/iOS/source/MapboxSDK.iOS/Additions.cs
@@ -27,28 +27,75 @@ namespace Mapbox
 
     partial class Polygon 
     {
+
         public static Polygon WithCoordinates (CLLocationCoordinate2D[] coords, nuint count)
         {
-			return WithCoordinates(coords, count);
-            //return WithCoordinates (GetPointer (coords), count);
+            return WithCoordinates(GetPointer(coords), count);
         }
 
-        internal static IntPtr GetPointer (CLLocationCoordinate2D[] coordinates)
+		unsafe internal static IntPtr GetPointer (CLLocationCoordinate2D[] coordinates)
         {
             fixed (CLLocationCoordinate2D* ptr = &coordinates[0])
             return (IntPtr) ptr;
         }            
     }
 
+	partial class MultiPoint {
+		public CLLocationCoordinate2D[] GetCoordinates() { 
+			return PtrToCLLocationCoordinate2DArray(this.Coordinates);
+		}
+
+		internal static CLLocationCoordinate2D[] PtrToCLLocationCoordinate2DArray(IntPtr cllocationCoordinate2DArray)
+		{
+			if (cllocationCoordinate2DArray == IntPtr.Zero)
+				return new CLLocationCoordinate2D[] { };
+
+
+			var argc = CountCLLocationCoordinate2D(cllocationCoordinate2DArray);
+			return PtrToCLLocationCoordinate2DArray(argc, cllocationCoordinate2DArray);
+		}
+
+		static int CountCLLocationCoordinate2D(IntPtr cllocationCoordinate2DArray)
+		{
+			var structSize = Marshal.SizeOf(typeof(CLLocationCoordinate2D));
+
+			int count = 0;
+			while (Marshal.ReadIntPtr(cllocationCoordinate2DArray, count * structSize) != IntPtr.Zero)
+				++count;
+			return count;
+		}
+
+		static CLLocationCoordinate2D[] PtrToCLLocationCoordinate2DArray(int count, IntPtr cllocationCoordinate2DArray)
+		{
+			if (count < 0)
+				throw new ArgumentOutOfRangeException(nameof(count), "< 0");
+			if (cllocationCoordinate2DArray == IntPtr.Zero)
+				return new CLLocationCoordinate2D[count];
+
+			int structSize = Marshal.SizeOf(typeof(CLLocationCoordinate2D));
+
+			var members = new CLLocationCoordinate2D[count];
+
+			for (int i = 0; i < count; ++i)
+			{
+				var data = IntPtr.Add(cllocationCoordinate2DArray, structSize * i);
+				members[i] = Marshal.PtrToStructure<CLLocationCoordinate2D>(data);
+			}
+
+
+			return members;
+		}
+	}
+
     partial class Polyline 
     {
+
         public static Polyline WithCoordinates (CLLocationCoordinate2D[] coords, nuint count)
         {
-			return WithCoordinates(coords, count);
-			//return WithCoordinates(GetPointer(coords), count);
+			return WithCoordinates(GetPointer(coords), count);
         }
 
-        internal static IntPtr GetPointer (CLLocationCoordinate2D[] coordinates)
+        unsafe internal static IntPtr GetPointer (CLLocationCoordinate2D[] coordinates)
         {
             fixed (CLLocationCoordinate2D* ptr = &coordinates[0])
             return (IntPtr) ptr;
@@ -68,11 +115,10 @@ namespace Mapbox
 
         public void SetVisibleCoordinates (CLLocationCoordinate2D[] coordinates, nuint count, UIEdgeInsets insets, bool animated)
         {
-			//SetVisibleCoordinates(GetPointer(coordinates), count, insets, animated);
-            SetVisibleCoordinates (coordinates, count, insets, animated);
+			SetVisibleCoordinates(GetPointer(coordinates), count, insets, animated);
       }
 
-        internal static IntPtr GetPointer (CLLocationCoordinate2D[] coordinates)
+        unsafe internal static IntPtr GetPointer (CLLocationCoordinate2D[] coordinates)
         {
             fixed (CLLocationCoordinate2D* ptr = &coordinates[0])
             return (IntPtr) ptr;
