f4520e5a13a4e84d4bae5218b6c9e1a9b1548d1b
Bill Holmes <bill.holmes@xamarin.com>
2017-05-03 14:00:54 -0400

    [Mono.Posix] Use msvcrt for net standard but c for unix runtimes
    The NuGet package will now contain 2 versions of Mono.Posix.dll.  The general
    lib/netstandard2.0 version will point to msvcrt for PInvokes.  In the runtime
    section of the package for unix and osx systems a different version Mono.Posix
    that uses libc for PInvokes will be used at runtime.

--- XPlat/Mono.Posix/dockerbuild.sh
+++ XPlat/Mono.Posix/dockerbuild.sh
@@ -3,4 +3,8 @@
 cd /hostdir/monorepo/mcs/class/Mono.Posix
 
 dotnet restore Mono.Posix-netstandard_2_0.csproj
-dotnet build Mono.Posix-netstandard_2_0.csproj
+dotnet build Mono.Posix-netstandard_2_0.csproj /p:ForceUseLibC=false -c Release
+cp bin/Release/netstandard2.0/Mono.Posix.dll ../../../../build/any
+dotnet clean Mono.Posix-netstandard_2_0.csproj -c Release
+dotnet build Mono.Posix-netstandard_2_0.csproj /p:ForceUseLibC=true -c Release
+cp bin/Release/netstandard2.0/Mono.Posix.dll ../../../../build/unix
