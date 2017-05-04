2fcd35130fc1a71c6b3dca70de6d01bc00d3df82
Bill Holmes <bill.holmes@xamarin.com>
2017-05-04 14:09:38 -0400

    Output nunit test results in nunit2 format so Jenkins can read it.

--- XPlat/Mono.Posix/dockertest.sh
+++ XPlat/Mono.Posix/dockertest.sh
@@ -8,5 +8,5 @@ dotnet restore Mono.Posix.Test-netstandard_2_0.copy.csproj
 dotnet add Mono.Posix.Test-netstandard_2_0.copy.csproj package -s packagesource Mono.Posix.Core -v $1
 dotnet restore Mono.Posix.Test-netstandard_2_0.copy.csproj
 dotnet build Mono.Posix.Test-netstandard_2_0.copy.csproj
-dotnet run -p Mono.Posix.Test-netstandard_2_0.copy.csproj
+dotnet run -p Mono.Posix.Test-netstandard_2_0.copy.csproj --result="TestResult-v2.xml;format=nunit2"
 rm Mono.Posix.Test-netstandard_2_0.copy.csproj
