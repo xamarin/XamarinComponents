10aab685ef8509615ce3e9a7f1fd71f9fa330436
Bill Holmes <bill.holmes@xamarin.com>
2017-05-04 09:19:31 -0400

    [Mono.Posix] Adding some logging to build to find CI issues

--- XPlat/Mono.Posix/dockerbuild.sh
+++ XPlat/Mono.Posix/dockerbuild.sh
@@ -4,7 +4,13 @@ cd /hostdir/monorepo/mcs/class/Mono.Posix
 
 dotnet restore Mono.Posix-netstandard_2_0.csproj
 dotnet build Mono.Posix-netstandard_2_0.csproj /p:ForceUseLibC=false -c Release
+echo cp bin/Release/netstandard2.0/Mono.Posix.dll ../../../../build/any
 cp bin/Release/netstandard2.0/Mono.Posix.dll ../../../../build/any
+echo ls ../../../../build/any
+ls ../../../../build/any
 dotnet clean Mono.Posix-netstandard_2_0.csproj -c Release
 dotnet build Mono.Posix-netstandard_2_0.csproj /p:ForceUseLibC=true -c Release
+echo cp bin/Release/netstandard2.0/Mono.Posix.dll ../../../../build/unix
 cp bin/Release/netstandard2.0/Mono.Posix.dll ../../../../build/unix
+echo ls ../../../../build/unix
+ls ../../../../build/unix
