8165e65bd4bbb15e334b8aa9f68354bb923baeb5
Bill Holmes <bill.holmes@xamarin.com>
2017-05-18 12:25:23 -0400

    [Mono.Posix] Rename the NuGet package to Mono.Posix.NETCore

--- XPlat/Mono.Posix/dockertest.sh
+++ XPlat/Mono.Posix/dockertest.sh
@@ -5,7 +5,7 @@ cd /hostdir/Test
 cp Mono.Posix.Test-netstandard_2_0.csproj Mono.Posix.Test-netstandard_2_0.copy.csproj
 
 dotnet restore Mono.Posix.Test-netstandard_2_0.copy.csproj
-dotnet add Mono.Posix.Test-netstandard_2_0.copy.csproj package -s packagesource Mono.Posix.Core -v $1
+dotnet add Mono.Posix.Test-netstandard_2_0.copy.csproj package -s packagesource Mono.Posix.NETCore -v $1
 dotnet restore Mono.Posix.Test-netstandard_2_0.copy.csproj
 dotnet build Mono.Posix.Test-netstandard_2_0.copy.csproj
 dotnet run -p Mono.Posix.Test-netstandard_2_0.copy.csproj --result="TestResult-v2.xml;format=nunit2"
