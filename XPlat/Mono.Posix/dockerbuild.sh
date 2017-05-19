a696c1abcbc89d9f3e93c891c5d146ea3b7c205d
Bill Holmes <bill.holmes@xamarin.com>
2017-05-19 14:45:35 -0400

    [Mono.Posix] Rename the Mono.Posix NETStandard dll to Mono.Posix.NETStandard.dll
    Since the NETStandard version will have a different/smaller API we are going to
    change the name of the assembly.  The ultimate goal is to also provide a type forwarding
    assembly for Mono.Posix.NETStandard.dll to the installed desktop version of Mono.Posix.dll.

--- XPlat/Mono.Posix/dockerbuild.sh
+++ XPlat/Mono.Posix/dockerbuild.sh
@@ -2,15 +2,15 @@
 
 cd /hostdir/monorepo/mcs/class/Mono.Posix
 
-dotnet restore Mono.Posix-netstandard_2_0.csproj
+dotnet restore Mono.Posix.NETStandard-netstandard_2_0.csproj
 
 mkdir -p build/any
-dotnet build Mono.Posix-netstandard_2_0.csproj /p:ForceUseLibC=false -c Release
-cp bin/Release/netstandard2.0/Mono.Posix.dll build/any/
+dotnet build Mono.Posix.NETStandard-netstandard_2_0.csproj /p:ForceUseLibC=false -c Release
+cp bin/Release/netstandard2.0/Mono.Posix.NETStandard.dll build/any/
 
-dotnet clean Mono.Posix-netstandard_2_0.csproj -c Release
+dotnet clean Mono.Posix.NETStandard-netstandard_2_0.csproj -c Release
 
 mkdir -p build/unix
-dotnet build Mono.Posix-netstandard_2_0.csproj /p:ForceUseLibC=true -c Release
-cp bin/Release/netstandard2.0/Mono.Posix.dll build/unix/
+dotnet build Mono.Posix.NETStandard-netstandard_2_0.csproj /p:ForceUseLibC=true -c Release
+cp bin/Release/netstandard2.0/Mono.Posix.NETStandard.dll build/unix/
 
--- XPlat/Mono.Posix/dockertest.sh
+++ XPlat/Mono.Posix/dockertest.sh
@@ -2,11 +2,11 @@
 
 cd /hostdir/Test
 
-cp Mono.Posix.Test-netstandard_2_0.csproj Mono.Posix.Test-netstandard_2_0.copy.csproj
+cp Mono.Posix.NETStandard.Test-netstandard_2_0.csproj Mono.Posix.NETStandard.Test-netstandard_2_0.copy.csproj
 
-dotnet restore Mono.Posix.Test-netstandard_2_0.copy.csproj
-dotnet add Mono.Posix.Test-netstandard_2_0.copy.csproj package -s packagesource Mono.Posix.NETStandard -v $1
-dotnet restore Mono.Posix.Test-netstandard_2_0.copy.csproj
-dotnet build Mono.Posix.Test-netstandard_2_0.copy.csproj
-dotnet run -p Mono.Posix.Test-netstandard_2_0.copy.csproj --result="TestResult-v2.xml;format=nunit2"
-rm Mono.Posix.Test-netstandard_2_0.copy.csproj
+dotnet restore Mono.Posix.NETStandard.Test-netstandard_2_0.copy.csproj
+dotnet add Mono.Posix.NETStandard.Test-netstandard_2_0.copy.csproj package -s packagesource Mono.Posix.NETStandard -v $1
+dotnet restore Mono.Posix.NETStandard.Test-netstandard_2_0.copy.csproj
+dotnet build Mono.Posix.NETStandard.Test-netstandard_2_0.copy.csproj
+dotnet run -p Mono.Posix.NETStandard.Test-netstandard_2_0.copy.csproj --result="TestResult-v2.xml;format=nunit2"
+rm Mono.Posix.NETStandard.Test-netstandard_2_0.copy.csproj
