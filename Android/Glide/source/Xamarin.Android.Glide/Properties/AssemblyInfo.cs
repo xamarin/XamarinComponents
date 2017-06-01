07126f9bfb6cb1ab865a09aa4c2a4c41bef57f81
Redth <jondick@gmail.com>
2017-06-01 15:17:32 -0400

    [glide] Fix copyright in assemblyinfo

--- Android/Glide/source/Xamarin.Android.Glide/Properties/AssemblyInfo.cs
+++ Android/Glide/source/Xamarin.Android.Glide/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using Android.App;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("${AuthorCopyright}")]
+[assembly: AssemblyCopyright("Microsoft Corporation")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
