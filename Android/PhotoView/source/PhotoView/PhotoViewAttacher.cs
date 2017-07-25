3d34c65b3df46cbd1c876e8fe81f9dcf637f9509
Matthew Leibowitz <mattleibow@live.com>
2017-07-25 07:01:29 +0200

    [PhotoView] Update to v1.3.1  - added .nuspec  - added external dependencies

---
+++ Android/PhotoView/source/PhotoView/PhotoViewAttacher.cs
@@ -0,0 +1,21 @@
+﻿using Android.Graphics;
+
+namespace ImageViews.Photo
+{
+	partial class PhotoViewAttacher
+	{
+		public Matrix DisplayMatrix
+		{
+			get
+			{
+				var matrix = new Matrix();
+				GetDisplayMatrix(matrix);
+				return matrix;
+			}
+			set
+			{
+				SetDisplayMatrix(value);
+			}
+		}
+	}
+}
