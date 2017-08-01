fb2d12416537260d6b8fdebf69d22f8d181c7e28
Matthew Leibowitz <mattleibow@live.com>
2017-08-01 22:58:47 +0200

    [Kotlin] Using more Kotlin

--- Android/Kotlin/native/KotlinSample/kotlinsamplelibrary/src/main/java/com/example/kotlinsamplelibrary/TestClass.kt
+++ Android/Kotlin/native/KotlinSample/kotlinsamplelibrary/src/main/java/com/example/kotlinsamplelibrary/TestClass.kt
@@ -5,9 +5,7 @@ import java.util.ArrayList
 class TestClass {
     fun Test(array: Array<String>): List<String> {
         val list = ArrayList<String>()
-        for (str in array) {
-            list.add(str)
-        }
+        array.forEach { str -> list.add(str) }
         return list
     }
 }
