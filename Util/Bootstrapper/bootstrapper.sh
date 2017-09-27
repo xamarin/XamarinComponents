0fc1b7415c7290acf4f1c4adfce8028f8846dd2c
Redth <jondick@gmail.com>
2017-09-27 14:50:26 -0400

    [build] bootstrapper improvements

--- Util/Bootstrapper/bootstrapper.sh
+++ Util/Bootstrapper/bootstrapper.sh
@@ -9,13 +9,18 @@ if [ -f "$SCRIPT.commit" ]; then commitTxt=$(cat "$SCRIPT.commit"); fi
 
 # If the cached file's commit doesn't match our desired, or if it's 'master' go download the right version
 if [[ "$commitTxt" != "$COMMIT" ||  "$commitTxt" == "master" ]]; then
+
     echo "Downloading $SCRIPT..."
+
+    # Download the script
     curl -Lsfo $SCRIPT $URL
-    
     if [ $? -ne 0 ]; then
         echo "An error occured while downloading $SCRIPT."
         exit 1
     fi
+
+    # Write out the commit hash used to cache for future runs
+    echo $COMMIT > "$SCRIPT.commit"
 fi
 
 export BOOTSTRAPPER_COMMIT="$COMMIT"
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-COMMIT="846f697d61c542feb7aaf9cdd5aad981603d5722"
+COMMIT="443e9b8c6efb624177cdc0cea3c750e4bfff1910"
 URL="https://raw.githubusercontent.com/xamarin/XamarinComponents/$COMMIT/Util/Bootstrapper/cake.sh"
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 SCRIPT="$SCRIPT_DIR/cake.sh"
@@ -9,13 +9,18 @@ if [ -f "$SCRIPT.commit" ]; then commitTxt=$(cat "$SCRIPT.commit"); fi
 
 # If the cached file's commit doesn't match our desired, or if it's 'master' go download the right version
 if [[ "$commitTxt" != "$COMMIT" ||  "$commitTxt" == "master" ]]; then
+
     echo "Downloading $SCRIPT..."
+
+    # Download the script
     curl -Lsfo $SCRIPT $URL
-    
     if [ $? -ne 0 ]; then
         echo "An error occured while downloading $SCRIPT."
         exit 1
     fi
+
+    # Write out the commit hash used to cache for future runs
+    echo $COMMIT > "$SCRIPT.commit"
 fi
 
 export BOOTSTRAPPER_COMMIT="$COMMIT"
