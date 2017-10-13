77de76dc1d722035b3cd3e83cdbfe879ff5d9000
Bill Holmes <bill.holmes@xamarin.com>
2017-10-13 14:41:02 -0400

    [Estimote] Update the Android binding to 1.0.3
     - Fixing the sample so that it compiles
     - Mostly namespace changes with a few class name changes
     - Removing the Permissions file from the binding project
       as I believe that we now merge the AndroidManifest from the
       aar file

--- XPlat/Estimote/Android/source/Additions/Permissions.cs
+++
@@ -1,6 +0,0 @@
-﻿using Android;
-using Android.App;
-
-[assembly: UsesPermission(Manifest.Permission.Bluetooth)]
-[assembly: UsesPermission(Manifest.Permission.BluetoothAdmin)]
-[assembly: UsesFeature("android.hardware.bluetooth_le", Required = false)]
