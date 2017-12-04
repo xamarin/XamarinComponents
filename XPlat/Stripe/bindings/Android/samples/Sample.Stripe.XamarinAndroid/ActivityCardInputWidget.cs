604af046eb85bc3016ff108a0a93d428f322f3fa
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-12-04 15:48:35 +0100

    Stripe sample

---
+++ XPlat/Stripe/bindings/Android/samples/Sample.Stripe.XamarinAndroid/ActivityCardInputWidget.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Stripe.Android.View;
+
+namespace Sample.Stripe.XamarinAndroid
+{
+    [Activity(Label = "ActivityCardInputWidget")]
+    public class ActivityCardInputWidget : Activity
+    {
+        CardInputWidget mCardInputWidget = null;
+        Button buttonVerify = null;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.layout_card_input_widget);
+
+            mCardInputWidget = FindViewById<CardInputWidget>(Resource.Id.card_input_widget);
+            buttonVerify = FindViewById<Button>(Resource.Id.buttonVerify);
+
+            buttonVerify.Click += ButtonVerify_Click;
+
+            return;
+        }
+
+        void ButtonVerify_Click(object sender, EventArgs e)
+        {
+            global::Stripe.Android.Model.Card card = mCardInputWidget.Card;
+            if (card == null)
+            {
+                // Do not continue token creation.
+            }
+            else
+            {
+                global::Stripe.Android.Stripe stripe = null;
+                stripe = new global::Stripe.Android.Stripe(this, "pk_test_6pRNASCoBOKtIshFeQd4XMUh");
+                stripe.CreateToken
+                            (
+                                card,
+                                new TokenCallbackImplementation(this)
+                            );
+
+            }
+
+            return;
+        }
+    }
+}
---
+++ XPlat/Stripe/bindings/Android/samples/Sample.Stripe.XamarinAndroid/TokenCallbackImplementation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Java.Lang;
+using Stripe.Android.Model;
+
+namespace Sample.Stripe.XamarinAndroid
+{
+    public partial class TokenCallbackImplementation : Java.Lang.Object, global::Stripe.Android.ITokenCallback
+    {
+        Android.Content.Context context = null;
+
+        public TokenCallbackImplementation(Android.Content.Context c)
+        {
+            this.context = c;
+
+            return;
+        }
+
+        public void Dispose()
+        {
+            return;
+        }
+
+        public void OnError(Java.Lang.Exception error)
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            sb.AppendLine($"Error {error.LocalizedMessage}");
+
+            Android.Widget.Toast.MakeText
+                                    (
+                                        this.context, 
+                                        sb.ToString(), 
+                                        Android.Widget.ToastLength.Long
+                                    ).Show();
+
+            return;
+        }
+
+        public void OnSuccess(Token token)
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            sb.AppendLine($"Success send the token to your server");
+
+            Android.Widget.Toast.MakeText
+                                    (
+                                        this.context,
+                                        sb.ToString(),
+                                        Android.Widget.ToastLength.Long
+                                    ).Show();
+
+            return;
+        }
+    }
+}
