809ed13dd6df3d10e4385ca447f8c85faa84b2c3
Dave Humphreys <dave@thing.com>
2018-01-16 21:32:55 +0000

    Moved code over from repo and setup nuget and yaml
    Moved code over from repo and setup nuget and yaml

---
+++ iOS/PickerCells/samples/PickerCellSample/SamplePickerViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UIKit;
+
+namespace DatePickerCellSample
+{
+
+	public class SamplePickerViewModel : UIPickerViewModel 
+	{
+		static string [] names = new string [] {
+			"Brian Kernighan",
+			"Dennis Ritchie",
+			"Ken Thompson",
+			"Kirk McKusick",
+			"Rob Pike",
+			"Dave Presotto",
+			"Steve Johnson"
+		};
+
+
+		public SamplePickerViewModel () {
+			
+		}
+
+		public override nint GetComponentCount (UIPickerView v)
+		{
+			return 2;
+		}
+
+		public override nint GetRowsInComponent (UIPickerView pickerView, nint component)
+		{
+			return names.Length;
+		}
+
+		public override string GetTitle (UIPickerView picker, nint row, nint component)
+		{
+			if (component == 0)
+				return names [row];
+			else
+				return row.ToString ();
+		}
+
+		public override void Selected (UIPickerView picker, nint row, nint component)
+		{
+
+		}
+
+		public override nfloat GetComponentWidth (UIPickerView picker, nint component)
+		{
+			if (component == 0)
+				return 240f;
+			else
+				return 40f;
+		}
+
+		public override nfloat GetRowHeight (UIPickerView picker, nint component)
+		{
+			return 40f;
+		}
+	}
+}
+
---
+++ iOS/PickerCells/source/PickerCells/Data/PickerViewCellComponent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PickerCells.Data
+{
+	public class PickerViewCellComponent
+	{
+		public float Width
+		{
+			get;
+			set;
+		}
+
+		public List<PickerViewCellItem> Items
+		{
+			get;
+			set;
+		}
+
+		public PickerViewCellComponent()
+		{
+		}
+	}
+}
+
