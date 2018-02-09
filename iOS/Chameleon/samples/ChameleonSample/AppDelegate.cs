9c97d2f5cffa01b816d5b02a34ad2d616d24ae1c
adlair <adlair.cerecedo@xamarin.com>
2018-02-09 12:06:19 -0600

    [Chameleon] to NuGet

--- iOS/Chameleon/samples/ChameleonSample/AppDelegate.cs
+++ iOS/Chameleon/samples/ChameleonSample/AppDelegate.cs
@@ -5,22 +5,21 @@ using ChameleonFramework;
 
 namespace ChameleonSample
 {
-	[Register ("AppDelegate")]
-	public partial class AppDelegate : UIApplicationDelegate
-	{
-		public override UIWindow Window { get; set; }
+    [Register("AppDelegate")]
+    public partial class AppDelegate : UIApplicationDelegate
+    {
+        public override UIWindow Window { get; set; }
 
-		public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
-		{
-			// get the strong colors as the theme
-			var image = UIImage.FromBundle ("africa-blue.jpg");
-			var colors = ChameleonColorArray.GetColors (image, false);
-			Chameleon.SetGlobalTheme (
-				colors [2],
-				colors [4],
-				ContentStyle.Contrast);
-
-			return true;
-		}
-	}
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            // get the strong colors as the theme
+            var image = UIImage.FromBundle("africa-blue.jpg");
+            var colors = ChameleonColorArray.GetColors(image, false);
+            Chameleon.SetGlobalTheme(
+                colors[2],
+                colors[4],
+                (ChameleonFramework.UIContentStyle)ContentStyle.Contrast);
+            return true;
+        }
+    }
 }
