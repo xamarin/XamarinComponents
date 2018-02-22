e10b325cc12170483618ed37261f9694ec68b139
Redth <jondick@gmail.com>
2018-02-22 10:02:49 -0500

    [android-wear] Moved and updated to latest binding

---
+++ Android/Wear/samples/WatchFaceSample/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace WatchFaceSample
+{
+    [Activity (Label = "WatchFaceSample", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate (Bundle bundle)
+        {
+            base.OnCreate (bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
+
