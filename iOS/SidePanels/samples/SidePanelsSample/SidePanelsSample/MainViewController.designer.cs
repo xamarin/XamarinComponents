2073f21980ac1f8cb7d836447b4149dc34c9e777
Adlair Cerecedo Mendez <adlair@Adlairs-MacBook-Pro.local>
2018-04-20 01:15:20 -0500

    iPhone X support added.

--- iOS/SidePanels/samples/SidePanelsSample/SidePanelsSample/MainViewController.designer.cs
+++ iOS/SidePanels/samples/SidePanelsSample/SidePanelsSample/MainViewController.designer.cs
@@ -1,6 +1,6 @@
 // WARNING
 //
-// This file has been generated automatically by Xamarin Studio from the outlets and
+// This file has been generated automatically by Visual Studio from the outlets and
 // actions declared in your storyboard file.
 // Manual changes to this file will not be maintained.
 //
@@ -11,10 +11,10 @@ using UIKit;
 
 namespace SidePanelsSample
 {
-    [Register("MainViewController")]
+    [Register ("MainViewController")]
     partial class MainViewController
     {
-        void ReleaseDesignerOutlets()
+        void ReleaseDesignerOutlets ()
         {
         }
     }
