01841f83375ec9c5002977095bfb7b4c14eb697a
Adlair Cerecedo Mendez <adlair@Virt-Node2.R2CSRM.com>
2018-05-10 13:10:34 -0500

    Formatting issues fixed.

--- iOS/Chameleon/samples/ChameleonSample/AppDelegate.cs
+++ iOS/Chameleon/samples/ChameleonSample/AppDelegate.cs
@@ -7,17 +7,17 @@ namespace ChameleonSample
     [Register("AppDelegate")]
     public partial class AppDelegate : UIApplicationDelegate
     {
-        public override UIWindow Window { get; set; }
-        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
-        {
-            // get the strong colors as the theme
-            var image = UIImage.FromBundle("africa-blue.jpg");
-            var colors = ChameleonColorArray.GetColors(image, false);
-            Chameleon.SetGlobalTheme(
-                colors[2],
-                colors[4],
-                (ChameleonFramework.UIContentStyle)ContentStyle.Contrast);
-            return true;
-        }
-    }
+		public override UIWindow Window { get; set; }
+		public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+		{
+			// get the strong colors as the theme
+			var image = UIImage.FromBundle("africa-blue.jpg");
+			var colors = ChameleonColorArray.GetColors(image, false);
+			Chameleon.SetGlobalTheme(
+				colors[2],
+				colors[4],
+				(ChameleonFramework.UIContentStyle)ContentStyle.Contrast);
+			return true;
+		}
+	}
 }
