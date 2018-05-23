bd23a0d181148a50bf861e4aa7d71993b78daee3
Adlair Cerecedo Mendez <adlair@Adlairs-MacBook-Pro.local>
2018-05-23 00:50:21 -0500

    [AnimatedShapeButton] to Nuget

--- iOS/AnimatedShapeButton/samples/AnimatedShapeButtonSample/AnimatedShapeButtonSample/ViewController.designer.cs
+++ iOS/AnimatedShapeButton/samples/AnimatedShapeButtonSample/AnimatedShapeButtonSample/ViewController.designer.cs
@@ -1,6 +1,6 @@
 // WARNING
 //
-// This file has been generated automatically by Xamarin Studio from the outlets and
+// This file has been generated automatically by Visual Studio from the outlets and
 // actions declared in your storyboard file.
 // Manual changes to this file will not be maintained.
 //
@@ -11,43 +11,46 @@ using UIKit;
 
 namespace AnimatedShapeButtonSample
 {
-	[Register ("ViewController")]
-	partial class ViewController
-	{
-		[Outlet]
-		[GeneratedCode ("iOS Designer", "1.0")]
-		AnimatedButtons.AnimatedShapeButton heartButton { get; set; }
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        AnimatedButtons.AnimatedShapeButton heartButton { get; set; }
 
-		[Outlet]
-		[GeneratedCode ("iOS Designer", "1.0")]
-		AnimatedButtons.AnimatedShapeButton likeButton { get; set; }
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        AnimatedButtons.AnimatedShapeButton likeButton { get; set; }
 
-		[Outlet]
-		[GeneratedCode ("iOS Designer", "1.0")]
-		AnimatedButtons.AnimatedShapeButton smileButton { get; set; }
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        AnimatedButtons.AnimatedShapeButton smileButton { get; set; }
 
-		[Outlet]
-		[GeneratedCode ("iOS Designer", "1.0")]
-		AnimatedButtons.AnimatedShapeButton starButton { get; set; }
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        AnimatedButtons.AnimatedShapeButton starButton { get; set; }
 
-		void ReleaseDesignerOutlets ()
-		{
-			if (heartButton != null) {
-				heartButton.Dispose ();
-				heartButton = null;
-			}
-			if (likeButton != null) {
-				likeButton.Dispose ();
-				likeButton = null;
-			}
-			if (smileButton != null) {
-				smileButton.Dispose ();
-				smileButton = null;
-			}
-			if (starButton != null) {
-				starButton.Dispose ();
-				starButton = null;
-			}
-		}
-	}
+        void ReleaseDesignerOutlets ()
+        {
+            if (heartButton != null) {
+                heartButton.Dispose ();
+                heartButton = null;
+            }
+
+            if (likeButton != null) {
+                likeButton.Dispose ();
+                likeButton = null;
+            }
+
+            if (smileButton != null) {
+                smileButton.Dispose ();
+                smileButton = null;
+            }
+
+            if (starButton != null) {
+                starButton.Dispose ();
+                starButton = null;
+            }
+        }
+    }
 }
