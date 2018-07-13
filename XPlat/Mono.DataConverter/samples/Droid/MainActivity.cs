944d6d36f78cfa2ae818874a2eab90a1e63059ca
Matthew Leibowitz <mattleibow@live.com>
2018-07-13 16:19:51 +0200

    [mono.dataconverter] Updated the library to v5.12.0.273  - using a modern project system (sdk-style with multitargeting)  - using auto nuget packing

--- XPlat/Mono.DataConverter/samples/Droid/MainActivity.cs
+++ XPlat/Mono.DataConverter/samples/Droid/MainActivity.cs
@@ -1,26 +1,21 @@
-﻿using System;
-
-using Android.App;
-using Android.Content;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
 
 namespace DataConverterSample.Droid
 {
-	[Activity (Label = "DataConverterSample.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	[Activity (Label = "Data Converter", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : FormsApplicationActivity
 	{
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 
-			global::Xamarin.Forms.Forms.Init (this, bundle);
+			Forms.Init (this, bundle);
 
 			LoadApplication (new App ());
 		}
 	}
 }
-
