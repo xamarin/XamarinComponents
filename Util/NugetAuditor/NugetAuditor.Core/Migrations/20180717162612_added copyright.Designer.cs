bc3ba83736acc9cc91936d13ecd23a28aff257bc
newky2k <dave@git.com>
2018-07-17 17:27:12 +0100

    Added copyright

--- Util/NugetAuditor/NugetAuditor.Core/Migrations/20180717162612_added copyright.Designer.cs
+++ Util/NugetAuditor/NugetAuditor.Core/Migrations/20180717162612_added copyright.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NugetAuditor.Core;
 
 namespace NugetAuditor.Core.Migrations
 {
     [DbContext(typeof(AuditorDbContext))]
-    partial class AuditorDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180717162612_added copyright")]
+    partial class addedcopyright
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
