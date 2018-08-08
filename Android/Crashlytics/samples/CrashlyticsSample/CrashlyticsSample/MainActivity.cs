c1941a13a69e4aa101d4404150851fec4cec5036
adlair <adlair.cerecedo@xamarin.com>
2018-08-07 23:12:13 -0500

    New sample fixed, Crashlytics fixed.

--- Android/Crashlytics/samples/CrashlyticsSample/CrashlyticsSample/MainActivity.cs
+++ Android/Crashlytics/samples/CrashlyticsSample/CrashlyticsSample/MainActivity.cs
@@ -1,8 +1,6 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
-using Crashlytics.Devtools;
-using Fabric;
 using Java.Lang;
 using Android.Views;
 
@@ -14,14 +12,34 @@ namespace CrashlyticsSample
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            //Fabric.Fabric.With(this, new Crashlytics.Devtools.Crashlytics());
+            Fabric.Fabric.With(this, new Crashlytics.Crashlytics());
             SetContentView(Resource.Layout.Main);
-        }
+            var btnForceCrash = FindViewById<Button>(Resource.Id.btnCrashlytics);
+            var btnLogin = FindViewById<Button>(Resource.Id.btnFabric);
 
-        public void ForceCrash(View view)
-        {
-            throw new RuntimeException("This is a crash");
-        }
+            btnLogin.Click += (sender, args) =>
+            {
+                // TODO: Use the current user's information
+                // You can call any combination of these three methods
+                Crashlytics.Crashlytics.SetUserIdentifier("12345");
+                Crashlytics.Crashlytics.SetUserIdentifier("user@fabric.io");
+                Crashlytics.Crashlytics.SetUserName("Test User");
+            };
 
+            btnForceCrash.Click += (sender, args) =>
+            {
+                AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                alert.SetTitle("Force Crash");
+                alert.SetMessage("This is a Crash!");
+                alert.SetNegativeButton("Ok", (senderAlert, EventArgs) =>
+                {
+                    Toast.MakeText(this, "Ok", ToastLength.Short).Show();
+                });
+
+                Dialog dialog = alert.Create();
+                dialog.Show();
+            };
+
+        }
     }
 }
