8f62334b618cad27175f034cc8480c4d7d0887dd
SotoiGhost <issoto@microsoft.com>
2018-08-28 21:35:02 -0500

    [iOS][MaterialComponents] Added sample

--- iOS/MaterialComponents/samples/MaterialSample/AppDelegate.cs
+++ iOS/MaterialComponents/samples/MaterialSample/AppDelegate.cs
@@ -1,57 +1,43 @@
-﻿using Foundation;
+﻿using System;
+using System.Runtime.CompilerServices;
+
+using Foundation;
 using UIKit;
 
-namespace MaterialSample
-{
+using MaterialComponents;
+
+namespace MaterialSample {
 	// The UIApplicationDelegate for the application. This class is responsible for launching the
 	// User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
 	[Register ("AppDelegate")]
-	public class AppDelegate : UIApplicationDelegate
-	{
+	public class AppDelegate : UIApplicationDelegate, IAppBarNavigationControllerDelegate, IUINavigationControllerDelegate {
 		// class-level declarations
 
-		public override UIWindow Window
-		{
-			get;
-			set;
-		}
+		public override UIWindow Window { get; set; }
 
-		public override bool FinishedLaunching (UIApplication application,NSDictionary launchOptions)
+		public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
 		{
-			var tt = new MaterialComponents.CardScheme ().SemanticColorScheme;
+			UIApplication.SharedApplication.StatusBarStyle = UIStatusBarStyle.LightContent;
+
+			(Window.RootViewController as UINavigationController).Delegate = this;
+
 			return true;
 		}
 
-		public override void OnResignActivation (UIApplication application)
+		public static void LogMessage (string message, string className, [CallerMemberName] string methodName = "", [CallerLineNumber] int lineNumber = 0)
+		=> Console.WriteLine ($"{className}.{methodName} line {lineNumber}: {message}");
+
+		#region UINavigationController Delegate
+
+		[Export ("navigationController:willShowViewController:animated:")]
+		public void WillShowViewController (UINavigationController navigationController, UIViewController viewController, bool animated)
 		{
-			// Invoked when the application is about to move from active to inactive state.
-			// This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) 
-			// or when the user quits the application and it begins the transition to the background state.
-			// Games should use this method to pause the game.
+			var btnBack = new UIBarButtonItem (" ", UIBarButtonItemStyle.Plain, null);
+			viewController.NavigationItem.BackBarButtonItem = btnBack;
 		}
 
-		public override void DidEnterBackground (UIApplication application)
-		{
-			// Use this method to release shared resources, save user data, invalidate timers and store the application state.
-			// If your application supports background exection this method is called instead of WillTerminate when the user quits.
-		}
+		#endregion
 
-		public override void WillEnterForeground (UIApplication application)
-		{
-			// Called as part of the transiton from background to active state.
-			// Here you can undo many of the changes made on entering the background.
-		}
-
-		public override void OnActivated (UIApplication application)
-		{
-			// Restart any tasks that were paused (or not yet started) while the application was inactive. 
-			// If the application was previously in the background, optionally refresh the user interface.
-		}
-
-		public override void WillTerminate (UIApplication application)
-		{
-			// Called when the application is about to terminate. Save data, if needed. See also DidEnterBackground.
-		}
 	}
 }
 
---
+++ iOS/MaterialComponents/samples/MaterialSample/MenuCollectionViewController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+using CoreGraphics;
+using Foundation;
+using UIKit;
+
+using MaterialComponents;
+
+namespace MaterialSample {
+	public partial class MenuCollectionViewController : UICollectionViewController, IUICollectionViewDelegateFlowLayout, IInkTouchControllerDelegate {
+		InkTouchController inkTouchController;
+
+		protected MenuCollectionViewController (IntPtr handle) : base (handle) { }
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			Title = "Material Components for iOS";
+
+			if (UIDevice.CurrentDevice.CheckSystemVersion (11, 0))
+				CollectionView.ContentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentBehavior.Always;
+
+			CollectionView.BackgroundColor = UIColor.FromWhiteAlpha (0.9f, 1);
+
+			inkTouchController = new InkTouchController (CollectionView) {
+				DelaysInkSpread = true,
+				Delegate = this
+			};
+			inkTouchController.AddInkView ();
+		}
+
+		#region Internal Functionality
+
+		InkView GetInkView (UIView view)
+		{
+			var foundInkView = InkView.GetInjectedInkView (view);
+			foundInkView.InkStyle = InkStyle.Bounded;
+			foundInkView.InkColor = UIColor.FromWhiteAlpha (0.9f, 0.3f);
+			return foundInkView;
+		}
+
+		#endregion
+
+		#region UICollectionView Data Source
+
+		[Export ("numberOfSectionsInCollectionView:")]
+		public new nint NumberOfSections (UICollectionView collectionView) => 1;
+		public override nint GetItemsCount (UICollectionView collectionView, nint section) => SamplesManager.SharedInstance.Components.Length;
+
+		public override UICollectionViewCell GetCell (UICollectionView collectionView, NSIndexPath indexPath)
+		{
+			var component = SamplesManager.SharedInstance.Components [indexPath.Row];
+
+			var cell = collectionView.DequeueReusableCell (MenuCollectionViewCell.Key, indexPath) as MenuCollectionViewCell;
+			cell.BackgroundColor = UIColor.White;
+			cell.SampleTitle = component.Title;
+
+			InkView.GetInjectedInkView (View).CancelAllAnimations (false);
+
+			return cell;
+		}
+
+		#endregion
+
+		#region UICollectionView Delegate
+
+		[Export ("collectionView:didSelectItemAtIndexPath:")]
+		public new void ItemSelected (UICollectionView collectionView, NSIndexPath indexPath)
+		{
+			var viewController = Storyboard.InstantiateViewController (nameof (SamplesListTableViewController)) as SamplesListTableViewController;
+			viewController.Component = SamplesManager.SharedInstance.Components [indexPath.Row];
+			NavigationController.PushViewController (viewController, true);
+		}
+
+		#endregion
+
+		#region UICollectionView Delegate Flow Layout
+
+		[Export ("collectionView:layout:sizeForItemAtIndexPath:")]
+		public CGSize GetSizeForItem (UICollectionView collectionView, UICollectionViewLayout layout, NSIndexPath indexPath)
+		{
+			var dividerWidth = 1;
+			var safeInsets = 0d;
+
+			if (UIDevice.CurrentDevice.CheckSystemVersion (11, 0))
+				safeInsets = View.SafeAreaInsets.Left + View.SafeAreaInsets.Right;
+
+			var columns = 0;
+			if (UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone)
+				columns = UIDevice.CurrentDevice.Orientation == UIDeviceOrientation.Portrait ? 2 : 3;
+			else
+				columns = 4;
+
+			var dividers = columns + 1;
+			var cellWidth = (View.Frame.Width - dividers * dividerWidth - safeInsets) / columns;
+			return new CGSize (cellWidth, cellWidth);
+		}
+
+		#endregion
+
+		#region InkTouchController Delegate
+
+		[Export ("inkTouchController:shouldProcessInkTouchesAtTouchLocation:")]
+		public bool ShouldProcessInkTouches (InkTouchController inkTouchController, CGPoint location) => CollectionView.IndexPathForItemAtPoint (location) != null;
+
+		[Export ("inkTouchController:inkViewAtTouchLocation:")]
+		InkView GetInkViewAtTouchLocation (InkTouchController inkTouchController, CGPoint location)
+		{
+			var indexPath = CollectionView.IndexPathForItemAtPoint (location);
+
+			if (indexPath == null)
+				return new InkView ();
+
+			var cell = CollectionView.CellForItem (indexPath);
+			return GetInkView (cell);
+		}
+
+		#endregion
+	}
+}
+
