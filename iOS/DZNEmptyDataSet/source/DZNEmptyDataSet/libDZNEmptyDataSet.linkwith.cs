7fc55516c60a589685e66c55f088478dfa6a7404
Wenfengcheng <wenfeng.funkyxamarin@gmail.com>
2019-02-01 14:10:05 +0800

    use .a file instead of .framework

---
+++ iOS/DZNEmptyDataSet/source/DZNEmptyDataSet/libDZNEmptyDataSet.linkwith.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+#if __UNIFIED__
+using ObjCRuntime;
+#else
+using MonoTouch.ObjCRuntime;
+#endif
+
+[assembly: AssemblyVersion("1.8.1")]
+[assembly: AssemblyFileVersion("1.8.1")]
+
+[assembly: LinkWith(
+    "libDZNEmptyDataSet.a",
+    LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Arm64,
+    ForceLoad = true)]
+
