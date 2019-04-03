9ae1d89e03bb9941f24db77193b6242588a48e69
Dave Humphreys <newky2k@text.tst.com>
2019-04-03 17:10:47 +0100

    Added TranslationController

---
+++ Android/SceneForm/source/UX/TranslationController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Java.Interop;
+
+namespace Google.AR.Sceneform.UX
+{
+
+    public partial class TranslationController
+    {
+        
+        protected override unsafe void OnEndTransformation(global::Java.Lang.Object gesture)
+        {
+            const string __id = "onEndTransformation.(Ljava/lang/Object;)V";
+            try
+            {
+                JniArgumentValue* __args = stackalloc JniArgumentValue[1];
+                __args[0] = new JniArgumentValue((gesture == null) ? IntPtr.Zero : ((global::Java.Lang.Object)gesture).Handle);
+                _members.InstanceMethods.InvokeVirtualVoidMethod(__id, this, __args);
+            }
+            finally
+            {
+            }
+        }
+
+    }
+}
