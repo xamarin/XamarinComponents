3b327ea4e11ee4bd42d35b77b6da187c9fe68703
redth <jondick@gmail.com>
2019-05-06 11:20:15 -0400

    Added Office UI Fabric Android bindings

---
+++ XPlat/OfficeUIFabric/Android/build.cake
@@ -0,0 +1,62 @@
+
+var TARGET = Argument ("t", Argument ("target", "Default"));
+
+var AAR_VERSION = "0.2.3";
+var NUGET_VERSION = AAR_VERSION;
+var AAR_URL = $"http://jcenter.bintray.com/com/microsoft/uifabric/OfficeUIFabric/{AAR_VERSION}/OfficeUIFabric-{AAR_VERSION}.aar";
+
+Task ("externals")
+	.WithCriteria (!FileExists ("./externals/OfficeUIFabric.aar"))
+	.Does (() =>
+{
+	EnsureDirectoryExists ("./externals");
+	
+	// Download Dependencies
+	DownloadFile (AAR_URL, "./externals/OfficeUIFabric.aar");
+
+	// Update .csproj nuget versions
+	XmlPoke("./source/OfficeUIFabric.Android.csproj", "/Project/PropertyGroup/PackageVersion", NUGET_VERSION);
+});
+
+
+Task("libs")
+	.IsDependentOn("externals")
+	.Does(() =>
+{
+	MSBuild("./OfficeUIFabric.sln", c => {
+		c.Configuration = "Release";
+		c.Restore = true;
+		c.MaxCpuCount = 0;
+		c.Properties.Add("DesignTimeBuild", new [] { "false" });
+	});
+});
+
+Task("nuget")
+	.IsDependentOn("libs")
+	.Does(() =>
+{
+	MSBuild ("./OfficeUIFabric.sln", c => {
+		c.Configuration = "Release";
+		c.MaxCpuCount = 0;
+		c.Targets.Clear();
+		c.Targets.Add("Pack");
+		c.Properties.Add("PackageOutputPath", new [] { MakeAbsolute(new FilePath("./output")).FullPath });
+		c.Properties.Add("PackageRequireLicenseAcceptance", new [] { "true" });
+		c.Properties.Add("DesignTimeBuild", new [] { "false" });
+	});
+});
+
+Task("samples")
+	.IsDependentOn("nuget");
+
+Task ("clean")
+	.Does (() =>
+{
+	if (DirectoryExists ("./externals/"))
+		DeleteDirectory ("./externals", new DeleteDirectorySettings {
+			Recursive = true,
+			Force = true
+		});
+});
+
+RunTarget (TARGET);
---
+++ XPlat/OfficeUIFabric/Android/source/Additions/Additions.cs
@@ -0,0 +1,47 @@
+﻿using Android.Runtime;
+using Java.Interop;
+using System;
+
+namespace Microsoft.OfficeUIFabric
+{
+
+    public partial class CalendarAdapter
+    {
+        // Metadata.xml XPath method reference: path="/api/package[@name='com.microsoft.officeuifabric.calendar']/class[@name='CalendarAdapter']/method[@name='onBindViewHolder' and count(parameter)=2 and parameter[1][@type='com.microsoft.officeuifabric.calendar.CalendarAdapter.CalendarDayViewHolder'] and parameter[2][@type='int']]"
+        [Register("onBindViewHolder", "(Lcom/microsoft/officeuifabric/calendar/CalendarAdapter$CalendarDayViewHolder;I)V", "")]
+        public override unsafe void OnBindViewHolder(global::Android.Support.V7.Widget.RecyclerView.ViewHolder holder, int position)
+        {
+            const string __id = "onBindViewHolder.(Lcom/microsoft/officeuifabric/calendar/CalendarAdapter$CalendarDayViewHolder;I)V";
+            try
+            {
+                JniArgumentValue* __args = stackalloc JniArgumentValue[2];
+                __args[0] = new JniArgumentValue((holder == null) ? IntPtr.Zero : ((global::Java.Lang.Object)holder).Handle);
+                __args[1] = new JniArgumentValue(position);
+                _members.InstanceMethods.InvokeAbstractVoidMethod(__id, this, __args);
+            }
+            finally
+            {
+            }
+        }
+    }
+
+    public partial class PersonaListAdapter
+    {
+        // Metadata.xml XPath method reference: path="/api/package[@name='com.microsoft.officeuifabric.calendar']/class[@name='PersonaListAdapter']/method[@name='onBindViewHolder' and count(parameter)=2 and parameter[1][@type='com.microsoft.officeuifabric.persona.PersonaListAdapter.ViewHolder'] and parameter[2][@type='int']]"
+        [Register("onBindViewHolder", "(Lcom/microsoft/officeuifabric/persona/PersonaListAdapter$ViewHolder;I)V", "")]
+        public override unsafe void OnBindViewHolder(global::Android.Support.V7.Widget.RecyclerView.ViewHolder holder, int position)
+        {
+            const string __id = "onBindViewHolder.(Lcom/microsoft/officeuifabric/persona/PersonaListAdapter$ViewHolder;I)V";
+            try
+            {
+                JniArgumentValue* __args = stackalloc JniArgumentValue[2];
+                __args[0] = new JniArgumentValue((holder == null) ? IntPtr.Zero : ((global::Java.Lang.Object)holder).Handle);
+                __args[1] = new JniArgumentValue(position);
+                _members.InstanceMethods.InvokeAbstractVoidMethod(__id, this, __args);
+            }
+            finally
+            {
+            }
+        }
+    }
+}
