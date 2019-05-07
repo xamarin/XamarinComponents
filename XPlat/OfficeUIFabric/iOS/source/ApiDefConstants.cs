a3d578f9f9648ab4acdb300e0238c774ce68564a
SotoiGhost <issoto@microsoft.com>
2019-05-07 11:15:28 -0500

    [XPlat][OfficeUIFabric][iOS] Added iOS bits

---
+++ XPlat/OfficeUIFabric/iOS/source/ApiDefConstants.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Microsoft.OfficeUIFabric {
+
+	internal static class ApiDefConstants {
+		public const string MSActionsCell = "_TtC14OfficeUIFabric13MSActionsCell";
+		public const string MSActivityIndicatorCell = "_TtC14OfficeUIFabric23MSActivityIndicatorCell";
+		public const string MSActivityIndicatorView = "_TtC14OfficeUIFabric23MSActivityIndicatorView";
+		public const string MSAvatarView = "_TtC14OfficeUIFabric12MSAvatarView";
+		public const string MSBadgeFieldDelegate = "_TtP14OfficeUIFabric20MSBadgeFieldDelegate_";
+		public const string MSBadgeField = "_TtC14OfficeUIFabric12MSBadgeField";
+		public const string MSBadgeViewDelegate = "_TtP14OfficeUIFabric19MSBadgeViewDelegate_";
+		public const string MSBadgeView = "_TtC14OfficeUIFabric11MSBadgeView";
+		public const string MSBadgeViewDataSource = "_TtC14OfficeUIFabric21MSBadgeViewDataSource";
+		public const string MSButton = "_TtC14OfficeUIFabric8MSButton";
+		public const string MSCalendarConfiguration = "_TtC14OfficeUIFabric23MSCalendarConfiguration";
+		public const string MSCenteredLabelCell = "_TtC14OfficeUIFabric19MSCenteredLabelCell";
+		public const string MSColors = "_TtC14OfficeUIFabric8MSColors";
+		public const string MSDateTimePicker = "_TtC14OfficeUIFabric16MSDateTimePicker";
+		public const string MSDateTimePickerDelegate = "_TtP14OfficeUIFabric24MSDateTimePickerDelegate_";
+		public const string MSDotView = "_TtC14OfficeUIFabric9MSDotView";
+		public const string MSDrawerController = "_TtC14OfficeUIFabric18MSDrawerController";
+		public const string MSDrawerControllerDelegate = "_TtP14OfficeUIFabric26MSDrawerControllerDelegate_";
+		public const string MSEasyTapButton = "_TtC14OfficeUIFabric15MSEasyTapButton";
+		public const string MSFonts = "_TtC14OfficeUIFabric7MSFonts";
+		public const string MSHud = "_TtC14OfficeUIFabric5MSHUD";
+		public const string MSHudDelegate = "_TtP14OfficeUIFabric13MSHUDDelegate_";
+		public const string MSHudParams = "_TtC14OfficeUIFabric11MSHUDParams";
+		public const string MSLabel = "_TtC14OfficeUIFabric7MSLabel";
+		public const string MSPageCardPresenterController = "_TtC14OfficeUIFabric29MSPageCardPresenterController";
+		public const string MSPersona = "_TtP14OfficeUIFabric9MSPersona_";
+		public const string MSTableViewCell = "_TtC14OfficeUIFabric15MSTableViewCell";
+		public const string MSPersonaCell = "_TtC14OfficeUIFabric13MSPersonaCell";
+		public const string MSPersonaData = "_TtC14OfficeUIFabric13MSPersonaData";
+		public const string MSPersonaListView = "_TtC14OfficeUIFabric17MSPersonaListView";
+		public const string MSPersonaListViewSearchDirectoryDelegate = "_TtP14OfficeUIFabric40MSPersonaListViewSearchDirectoryDelegate_";
+		public const string MSPopupMenuController = "_TtC14OfficeUIFabric21MSPopupMenuController";
+		public const string MSPopupMenuItem = "_TtC14OfficeUIFabric15MSPopupMenuItem";
+		public const string MSPopupMenuSection = "_TtC14OfficeUIFabric18MSPopupMenuSection";
+		public const string MSResizingHandleView = "_TtC14OfficeUIFabric20MSResizingHandleView";
+		public const string MSSegmentedControl = "_TtC14OfficeUIFabric18MSSegmentedControl";
+		public const string MSTouchForwardingView = "_TtC14OfficeUIFabric21MSTouchForwardingView";
+		public const string MSTwoLinesTitleView = "_TtC14OfficeUIFabric19MSTwoLinesTitleView";
+		public const string MSTwoLinesTitleViewDelegate = "_TtP14OfficeUIFabric27MSTwoLinesTitleViewDelegate_";
+		public const string OfficeUIFabricFramework = "_TtC14OfficeUIFabric23OfficeUIFabricFramework";
+	}
+}
+
---
+++ XPlat/OfficeUIFabric/iOS/source/Enums.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using ObjCRuntime;
+
+namespace Microsoft.OfficeUIFabric {
+
+	[Native]
+	public enum MSActivityIndicatorViewSize : long {
+		XSmall = 0,
+		Small = 1,
+		Medium = 2,
+		Large = 3,
+		XLarge = 4,
+	}
+
+	[Native]
+	public enum MSAvatarSize : long {
+		XSmall = 0,
+		Small = 1,
+		Medium = 2,
+		Large = 3,
+		XLarge = 4,
+		XxLarge = 5,
+	}
+
+	[Native]
+	public enum MSAvatarStyle : long {
+		Circle = 0,
+		Square = 1,
+	}
+
+	[Native]
+	public enum MSBadgeViewStyle : long {
+		Default = 0,
+		Warning = 1,
+		Error = 2,
+	}
+
+	[Native]
+	public enum MSButtonStyle : long {
+		PrimaryFilled = 0,
+		PrimaryOutline = 1,
+		SecondaryOutline = 2,
+		Borderless = 3,
+	}
+
+	[Native]
+	public enum MSDateStringCompactness : long {
+		LongDaynameDayMonth = 1,
+		LongDaynameDayMonthYear = 2,
+		ShortDayname = 3,
+		ShortDaynameShortMonthnameDay = 4,
+		ShortDaynameShortMonthnameDayFullYear = 5,
+		PartialDaynameShortDayMonth = 6,
+		LongDaynameDayMonthHoursColumnsMinutes = 7,
+		ShortDaynameShortMonthnameHoursColumnsMinutes = 8,
+		PartialDaynameShortDayMonthHoursColumsMinutes = 9,
+		PartialMonthnameDaynameFullYear = 10,
+		PartialMonthnameDaynameHoursColumnsMinutes = 11,
+		PartialMonthnameDayname = 12,
+		LongMonthNameFullYear = 13,
+		ShortDaynameHoursColumnMinutes = 14,
+		ShortDayMonth = 15,
+		LongDayMonthYearTime = 16,
+		ShortDaynameDayShortMonthYear = 17,
+	}
+
+	[Native]
+	public enum MSDateTimePickerMode : long {
+		Date = 0,
+		Time = 1,
+		Range = 2,
+		TimeRange = 3,
+	}
+
+	[Native]
+	public enum MSDrawerPresentationDirection : long {
+		Down = 0,
+		Up = 1,
+	}
+
+	[Native]
+	public enum MSDrawerResizingBehavior : long {
+		None = 0,
+		Dismiss = 1,
+		DismissOrExpand = 2,
+	}
+
+	[Native]
+	public enum MSTextColorStyle : long {
+		Regular = 0,
+		Secondary = 1,
+		White = 2,
+		Primary = 3,
+		Error = 4,
+		Warning = 5,
+	}
+
+	[Native]
+	public enum MSTextStyle : long {
+		Title1 = 0,
+		Title2 = 1,
+		Headline = 2,
+		Body = 3,
+		Subhead = 4,
+		Footnote = 5,
+		Caption1 = 6,
+		Caption2 = 7,
+	}
+
+	[Native]
+	public enum MSCustomViewSize : long {
+		Default = 0,
+		Zero = 1,
+		Small = 2,
+		Medium = 3,
+	}
+
+	[Native]
+	public enum MSTableViewCellAccessoryType : long {
+		None = 0,
+		DisclosureIndicator = 1,
+		DetailButton = 2,
+	}
+
+	[Native]
+	public enum MSPersonaListViewSelectionDirection : long {
+		Next = 1,
+		Prev = -1,
+	}
+
+	[Native]
+	public enum MSSeparatorOrientation : long {
+		Horizontal = 0,
+		Vertical = 1,
+	}
+
+	[Native]
+	public enum MSSeparatorStyle : long {
+		Default = 0,
+		Shadow = 1,
+	}
+
+	[Native]
+	public enum MSTimeStringCompactness : long {
+		ColumnsMinutes = 1,
+		MSTimeStringCompactnessHours = 2,
+	}
+
+	[Native]
+	public enum MSTwoLinesTitleStyle : long {
+		Light = 0,
+		Dark = 1,
+	}
+
+	[Native]
+	public enum MSTwoLinesTitleViewButtonStyle : long {
+		Disclosure = 0,
+		DownArrow = 1,
+	}
+
+	[Native]
+	public enum MSObscureStyle : long {
+		Blur = 0,
+		Dim = 1,
+	}
+
+
+}
