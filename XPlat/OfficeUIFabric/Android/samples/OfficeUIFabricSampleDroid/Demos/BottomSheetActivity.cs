4b9ac78ac11c32731f3a27751ae006a514bdfbaa
Jonathan Dick <jondick@gmail.com>
2019-08-01 14:38:18 -0400

    Added Android sample for Office UI Fabric

---
+++ XPlat/OfficeUIFabric/Android/samples/OfficeUIFabricSampleDroid/Demos/BottomSheetActivity.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Microsoft.OfficeUIFabric;
+
+namespace OfficeUIFabricSampleDroid.Demos
+{
+    [Activity]
+    public class BottomSheetActivity : DemoActivity, IOnBottomSheetItemClickListener
+    {
+        protected override int ContentLayoutId => Resource.Layout.activity_bottom_sheet;
+
+        View root_view;
+        Button show_with_single_line_items_button;
+        Button show_with_double_line_items_button;
+        Button show_bottom_sheet_dialog_button;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            root_view = FindViewById<View>(Resource.Id.root_view);
+            show_with_single_line_items_button = FindViewById<Button>(Resource.Id.show_with_single_line_items_button);
+            show_with_double_line_items_button = FindViewById<Button>(Resource.Id.show_with_double_line_items_button);
+            show_bottom_sheet_dialog_button = FindViewById<Button>(Resource.Id.show_bottom_sheet_dialog_button);
+
+            show_with_single_line_items_button.Click += delegate
+            {
+                var bottomSheet = BottomSheet.NewInstance(
+                    new List<BottomSheetItem> {
+                        new BottomSheetItem(
+                            Resource.Id.bottom_sheet_item_flag,
+                            Resource.Drawable.ic_flag,
+                            Resources.GetString(Resource.String.bottom_sheet_item_flag_title)
+                        ),
+                        new BottomSheetItem(
+                            Resource.Id.bottom_sheet_item_reply,
+                            Resource.Drawable.ic_reply,
+                            Resources.GetString(Resource.String.bottom_sheet_item_reply_title)
+                        ),
+                        new BottomSheetItem(
+                            Resource.Id.bottom_sheet_item_forward,
+                            Resource.Drawable.ic_forward,
+                            Resources.GetString(Resource.String.bottom_sheet_item_forward_title)
+                        ),
+                        new BottomSheetItem(
+                            Resource.Id.bottom_sheet_item_delete,
+                            Resource.Drawable.ic_trash_can,
+                            Resources.GetString(Resource.String.bottom_sheet_item_delete_title)
+                        )
+                    });
+                bottomSheet.Show(SupportFragmentManager, null);
+            };
+
+            show_with_double_line_items_button.Click += delegate {
+                var bottomSheet = BottomSheet.NewInstance(
+                    new List<BottomSheetItem> {
+                        new BottomSheetItem(
+                            Resource.Id.bottom_sheet_item_camera,
+                            Resource.Drawable.ic_camera,
+                            Resources.GetString(Resource.String.bottom_sheet_item_camera_title),
+                            Resources.GetString(Resource.String.bottom_sheet_item_camera_subtitle)
+                        ),
+                        new BottomSheetItem(
+                            Resource.Id.bottom_sheet_item_gallery,
+                            Resource.Drawable.ic_gallery,
+                            Resources.GetString(Resource.String.bottom_sheet_item_gallery_title),
+                            Resources.GetString(Resource.String.bottom_sheet_item_gallery_subtitle)
+                        ),
+                        new BottomSheetItem(
+                            Resource.Id.bottom_sheet_item_videos,
+                            Resource.Drawable.ic_videos,
+                            Resources.GetString(Resource.String.bottom_sheet_item_videos_title),
+                            Resources.GetString(Resource.String.bottom_sheet_item_videos_subtitle)
+                        ),
+                        new BottomSheetItem(
+                            Resource.Id.bottom_sheet_item_manage,
+                            Resource.Drawable.ic_wrench,
+                            Resources.GetString(Resource.String.bottom_sheet_item_manage_title),
+                            Resources.GetString(Resource.String.bottom_sheet_item_manage_subtitle)
+                        )
+                    });
+                bottomSheet.Show(SupportFragmentManager, null);
+            };
+
+            show_bottom_sheet_dialog_button.Click += delegate {
+                var bottomSheetDialog = new BottomSheetDialog(
+                    this,
+                    new List<BottomSheetItem> {
+                        new BottomSheetItem(
+                            Resource.Id.bottom_sheet_item_clock,
+                            Resource.Drawable.ic_clock,
+                            Resources.GetString(Resource.String.bottom_sheet_item_clock_title)
+                        ),
+                        new BottomSheetItem(
+                            Resource.Id.bottom_sheet_item_alarm,
+                            Resource.Drawable.ic_alarm,
+                            Resources.GetString(Resource.String.bottom_sheet_item_alarm_title)
+                        ),
+                        new BottomSheetItem(
+                            Resource.Id.bottom_sheet_item_stop_watch,
+                            Resource.Drawable.ic_stop_watch,
+                            Resources.GetString(Resource.String.bottom_sheet_item_stop_watch_title)
+                        ),
+                        new BottomSheetItem(
+                            Resource.Id.bottom_sheet_item_time_zone,
+                            Resource.Drawable.ic_time_zone,
+                            Resources.GetString(Resource.String.bottom_sheet_item_time_zone_title)
+                        )
+                    });
+                bottomSheetDialog.OnItemClickListener = this;
+                bottomSheetDialog.Show();
+            };
+        }
+        public void OnBottomSheetItemClick(int id)
+        {
+            switch (id) {
+                // single line items
+                case Resource.Id.bottom_sheet_item_flag:
+                    ShowSnackbar(Resources.GetString(Resource.String.bottom_sheet_item_flag_toast));
+                    break;
+                case Resource.Id.bottom_sheet_item_reply:
+                    ShowSnackbar(Resources.GetString(Resource.String.bottom_sheet_item_reply_toast));
+                    break;
+                case Resource.Id.bottom_sheet_item_forward:
+                    ShowSnackbar(Resources.GetString(Resource.String.bottom_sheet_item_forward_toast));
+                    break;
+                case Resource.Id.bottom_sheet_item_delete:
+                    ShowSnackbar(Resources.GetString(Resource.String.bottom_sheet_item_delete_toast));
+                    break;
+
+                // double line items
+                case Resource.Id.bottom_sheet_item_camera:
+                    ShowSnackbar(Resources.GetString(Resource.String.bottom_sheet_item_camera_toast));
+                    break;
+                case Resource.Id.bottom_sheet_item_gallery:
+                    ShowSnackbar(Resources.GetString(Resource.String.bottom_sheet_item_gallery_toast));
+                    break;
+                case Resource.Id.bottom_sheet_item_videos:
+                    ShowSnackbar(Resources.GetString(Resource.String.bottom_sheet_item_videos_toast));
+                    break;
+                case Resource.Id.bottom_sheet_item_manage:
+                    ShowSnackbar(Resources.GetString(Resource.String.bottom_sheet_item_manage_toast));
+                    break;
+
+                // dialog
+                case Resource.Id.bottom_sheet_item_clock:
+                    ShowSnackbar(Resources.GetString(Resource.String.bottom_sheet_item_clock_toast));
+                    break;
+                case Resource.Id.bottom_sheet_item_alarm:
+                    ShowSnackbar(Resources.GetString(Resource.String.bottom_sheet_item_alarm_toast));
+                    break;
+                case Resource.Id.bottom_sheet_item_stop_watch:
+                    ShowSnackbar(Resources.GetString(Resource.String.bottom_sheet_item_stop_watch_toast));
+                    break;
+                case Resource.Id.bottom_sheet_item_time_zone:
+                    ShowSnackbar(Resources.GetString(Resource.String.bottom_sheet_item_time_zone_toast));
+                    break;
+            }
+        }
+
+        void ShowSnackbar(string message)
+            => Snackbar.Companion.Instance.Make(root_view, message, Snackbar.LengthLong, Snackbar.Style.Regular).Show();
+    }
+}
---
+++ XPlat/OfficeUIFabric/Android/samples/OfficeUIFabricSampleDroid/Demos/Views/Cell.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Microsoft.OfficeUIFabric;
+
+namespace OfficeUIFabricSampleDroid.Demos.Views
+{
+    public enum CellOrientation
+    {
+        Horizontal,
+        Vertical
+    }
+
+    public class Cell : TemplateView
+    {
+        public Cell(Context context, Android.Util.IAttributeSet attrs, int defStyleAttr) : base (context, attrs, defStyleAttr)
+        {
+            if (attrs != null)
+            {
+                var styledAttrs = context.ObtainStyledAttributes(attrs, Resource.Styleable.Cell);
+                title = styledAttrs.GetString(Resource.Styleable.Cell_title);
+                description = styledAttrs.GetString(Resource.Styleable.Cell_description);
+                var orientationOrdinal = styledAttrs.GetInt(Resource.Styleable.Cell_orientation, (int)DefaultOrientation);
+                orientation = (CellOrientation)orientationOrdinal;
+                styledAttrs.Recycle();
+            }
+        }
+
+        public static CellOrientation DefaultOrientation = CellOrientation.Horizontal;
+
+        CellOrientation orientation = DefaultOrientation;
+
+        public CellOrientation Orientation {
+            get => orientation;
+            set {
+                orientation = value;
+                InvalidateTemplate();
+            }
+        }
+
+        string title = string.Empty;
+        public string Title
+        {
+            get => title;
+            set
+            {
+                title = value;
+                UpdateTemplate();
+            }
+        }
+
+        string description = string.Empty;
+        public string Description
+        {
+            get => description;
+            set
+            {
+                description = value;
+                UpdateTemplate();
+            }
+        }
+
+        protected override int TemplateId 
+            => orientation == CellOrientation.Horizontal 
+                ? Resource.Layout.template_cell_horizontal
+                : Resource.Layout.template_cell_vertical;
+
+
+        TextView titleView = null;
+        TextView descriptionView = null;
+
+
+        void UpdateTemplate()
+        {
+            if (titleView != null)
+                titleView.Text = title;
+            if (descriptionView != null)
+                descriptionView.Text = description;
+        }
+
+        protected override void OnTemplateLoaded()
+        {
+            base.OnTemplateLoaded();
+
+            titleView = FindViewInTemplateById(Resource.Id.cell_title) as TextView;
+            descriptionView = FindViewInTemplateById(Resource.Id.cell_description) as TextView;
+            UpdateTemplate();
+        }
+    }
+}
