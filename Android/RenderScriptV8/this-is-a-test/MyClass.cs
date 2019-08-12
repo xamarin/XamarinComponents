9a49f81581ceb8f8e5d3e78981aa1aa53f444b18
Matthew Leibowitz <mattleibow@live.com>
2019-08-12 22:41:34 +0200

    test3

--- Android/RenderScriptV8/this-is-a-test/MyClass.cs
+++ Android/RenderScriptV8/this-is-a-test/MyClass.cs
@@ -12,6 +12,26 @@ namespace thisisatest
 			return f.UnsafeDeserialize(null, null);
 		}
 
+		[Flags]
+		public enum Days : uint
+		{
+			None = 0,
+			Monday = 1,
+			Tuesday = 2,
+			Wednesday = 4,
+			Thursday = 8,
+			Friday = 16,
+			All = Monday | Tuesday | Wednesday | Thursday | Friday
+		}
+
+		public enum Color : sbyte
+		{
+			None = 0,
+			Red = 1,
+			Orange = 3,
+			Yellow = 4
+		}
+
 		public static object TestMethod()
 		{
 			RijndaelManaged rjMan = new RijndaelManaged();
