a86864294fd3785f76e6366e00de9fb415e7fadb
Matthew Leibowitz <mattleibow@live.com>
2019-08-18 04:08:30 +0200

    More

--- Android/Kotlin/samples/KotlinSample/MainActivity.cs
+++ Android/Kotlin/samples/KotlinSample/MainActivity.cs
@@ -4,11 +4,6 @@ using Android.OS;
 using Android.Support.V7.App;
 
 using KotlinSampleLibrary;
-using Java.Util;
-using Android.Runtime;
-using System.Linq;
-using System.Collections.Generic;
-using Kotlin.Collections;
 
 namespace KotlinSample
 {
@@ -26,93 +21,7 @@ namespace KotlinSample
 			var instance = new TestClass();
 			var list = instance.Test(new[] { "first", "second", "third" });
 
-			ICollection c = new Coll();
-			IList l = new List();
-			IMap m = new Map();
-			ISet s = new Set();
-
-			textView.Text = $"{s.Size()} = {s.Contains("one")}; {c.Size()} = {c.Contains("one")}; {l.Size()} = {l.Get(1)}; {m.Size()} = {m.Get(1)}.";
-		}
-	}
-
-	public class Set : AbstractMutableSet
-	{
-		private readonly JavaList<Java.Lang.Object> list = new JavaList<Java.Lang.Object> { "one", "two", "three" };
-
-		public override bool Add(Java.Lang.Object p0)
-		{
-			list.Add(p0);
-			return true;
-		}
-
-		public override int GetSize() => list.Size();
-
-		public override IIterator Iterator() => list.Iterator();
-	}
-
-	public class Coll : Kotlin.Collections.AbstractMutableCollection
-	{
-		private readonly JavaList<Java.Lang.Object> list = new JavaList<Java.Lang.Object> { "one", "two", "three" };
-
-		public override bool Add(Java.Lang.Object p0)
-		{
-			list.Add(p0);
-			return true;
-		}
-
-		public override int GetSize() => list.Size();
-
-		public override IIterator Iterator() => list.Iterator();
-	}
-
-	public class List : Kotlin.Collections.AbstractMutableList
-	{
-		private readonly JavaList<Java.Lang.Object> list = new JavaList<Java.Lang.Object> { "one", "two", "three" };
-
-		public override void Add(int p0, Java.Lang.Object p1) => list.Add(p0, p1);
-
-		public override Java.Lang.Object Get(int index) => list[index];
-
-		public override int GetSize() => list.Size();
-
-		public override IIterator Iterator() => list.Iterator();
-
-		public override Java.Lang.Object RemoveAt(int p0) => list.Remove(p0);
-
-		public override Java.Lang.Object Set(int p0, Java.Lang.Object p1) => list.Set(p0, p1);
-	}
-
-
-	public class Map : Kotlin.Collections.AbstractMap
-	{
-		private readonly JavaDictionary<Java.Lang.Object, Java.Lang.Object> list = new JavaDictionary<Java.Lang.Object, Java.Lang.Object>
-		{
-			{ 1, "one" },
-			{ 2, "two" },
-			{ 3, "three" }
-		};
-
-		public override System.Collections.ICollection Entries => list.Select(i => new Entry(i)).ToArray();
-
-		public class Entry : Java.Lang.Object, IMapEntry
-		{
-			private (Java.Lang.Object Key, Java.Lang.Object Value) i;
-
-			public Entry(KeyValuePair<Java.Lang.Object, Java.Lang.Object> i)
-			{
-				this.i = (i.Key, i.Value);
-			}
-
-			public Java.Lang.Object Key => i.Key;
-
-			public Java.Lang.Object Value => i.Value;
-
-			public Java.Lang.Object SetValue(Java.Lang.Object value)
-			{
-				var old = i.Value;
-				i = (i.Key, value);
-				return old;
-			}
+			textView.Text = $"There are {list.Count} items.";
 		}
 	}
 }
