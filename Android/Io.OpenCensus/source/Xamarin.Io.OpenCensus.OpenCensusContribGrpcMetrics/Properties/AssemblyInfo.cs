175c0a6cabfaa2a59579283d17891f058d5306f0
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2019-09-07 06:59:06 +0200

    © Microsoft Corporation. All rights reserved.

--- Android/Io.OpenCensus/source/Xamarin.Io.OpenCensus.OpenCensusContribGrpcMetrics/Properties/AssemblyInfo.cs
+++ Android/Io.OpenCensus/source/Xamarin.Io.OpenCensus.OpenCensusContribGrpcMetrics/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using Android.App;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft Corporation")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Microsoft Corporation 2019. All Rights Reserved")]
+[assembly: AssemblyCopyright("© Microsoft Corporation. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
