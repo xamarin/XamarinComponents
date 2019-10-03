8441fcba0a54fff8326b0707d1c2cd28920687aa
Jonathan Dick <jodick@microsoft.com>
2019-10-02 20:02:30 -0400

    Fix whitespace

--- Android/RenderScriptV8/samples/RenderScriptSample/MainActivity.cs
+++ Android/RenderScriptV8/samples/RenderScriptSample/MainActivity.cs
@@ -17,8 +17,8 @@ namespace RenderScriptSample
 	[Activity (Label = "@string/app_name", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
-		private ImageView _imageView;
-		private SeekBar _seekbar;
+		ImageView _imageView;
+		SeekBar _seekbar;
 
 		protected override void OnCreate (Bundle bundle)
 		{
@@ -39,47 +39,47 @@ namespace RenderScriptSample
 				// We don't want to blur, so just load the un-altered image.
 				_imageView.SetImageResource (Resource.Drawable.dog_and_monkeys);
 			} else {
-                BlurImage(radius);
+				BlurImage(radius);
 			}
 
 		}
 
-        void BlurImage(int radius)
-        {
-            // Disable the event handler and the Seekbar to prevent this from 
-            // happening multiple times.
-            _seekbar.ProgressChanged -= BlurImageHandler;
-            _seekbar.Enabled = false;
+		void BlurImage(int radius)
+		{
+			// Disable the event handler and the Seekbar to prevent this from 
+			// happening multiple times.
+			_seekbar.ProgressChanged -= BlurImageHandler;
+			_seekbar.Enabled = false;
 
-            _imageView.SetImageDrawable(null);
+			_imageView.SetImageDrawable(null);
 
-            Task.Run(() =>
-            {
-                // Load a clean bitmap and work from that.
-                var sentBitmap = BitmapFactory.DecodeResource(Resources, Resource.Drawable.dog_and_monkeys);
+			Task.Run(() =>
+			{
+				// Load a clean bitmap and work from that.
+				var sentBitmap = BitmapFactory.DecodeResource(Resources, Resource.Drawable.dog_and_monkeys);
 
-                var bitmap = sentBitmap.Copy(sentBitmap.GetConfig(), true);
-                var rs = RenderScript.Create(this);
-                var input = Allocation.CreateFromBitmap(rs, sentBitmap, Allocation.MipmapControl.MipmapNone, Allocation.UsageScript);
-                var output = Allocation.CreateTyped(rs, input.Type);
-                var script = ScriptIntrinsicBlur.Create(rs, Element.U8_4(rs));
-                script.SetRadius(radius);
-                script.SetInput(input);
-                script.ForEach(output);
-                output.CopyTo(bitmap);
-                // clean up renderscript resources
-                rs.Destroy();
-                input.Destroy();
-                output.Destroy();
-                script.Destroy();
+				var bitmap = sentBitmap.Copy(sentBitmap.GetConfig(), true);
+				var rs = RenderScript.Create(this);
+				var input = Allocation.CreateFromBitmap(rs, sentBitmap, Allocation.MipmapControl.MipmapNone, Allocation.UsageScript);
+				var output = Allocation.CreateTyped(rs, input.Type);
+				var script = ScriptIntrinsicBlur.Create(rs, Element.U8_4(rs));
+				script.SetRadius(radius);
+				script.SetInput(input);
+				script.ForEach(output);
+				output.CopyTo(bitmap);
+				// clean up renderscript resources
+				rs.Destroy();
+				input.Destroy();
+				output.Destroy();
+				script.Destroy();
 
-                RunOnUiThread(() =>
-                {
-                    _imageView.SetImageBitmap(bitmap);
-                    _seekbar.ProgressChanged += BlurImageHandler;
-                    _seekbar.Enabled = true;
-                });
-            });
-        }
+				RunOnUiThread(() =>
+				{
+				    _imageView.SetImageBitmap(bitmap);
+				    _seekbar.ProgressChanged += BlurImageHandler;
+				    _seekbar.Enabled = true;
+				});
+			});
+		}
 	}
 }
