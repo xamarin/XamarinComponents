fa93215946518193a85ac038b7e96634ab8b285b
Jonathan Dick <jodick@microsoft.com>
2019-12-20 09:41:09 -0500

    Add missing namespace

--- Util/Xamarin.AndroidBinderator/Xamarin.AndroidBinderator/Util.cs
+++ Util/Xamarin.AndroidBinderator/Xamarin.AndroidBinderator/Util.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
 
