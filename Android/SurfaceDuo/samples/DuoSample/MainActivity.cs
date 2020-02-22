477dfb586550860ad68e670f4630d207b1e1de96
Jonathan Dick <jondick@gmail.com>
2020-02-21 20:25:33 -0500

    Added a simple sample

---
+++ Android/SurfaceDuo/samples/DuoSample/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Microsoft.Device.Display;
+
+namespace DuoSample
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        ScreenHelper screenHelper;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            screenHelper = new ScreenHelper();
+            var isDuo = screenHelper.Initialize(this);
+
+            var isSpanned = screenHelper.IsDualMode;
+
+            FindViewById<TextView>(Resource.Id.textViewDuo).Text = $"Duo: {isDuo}";
+            FindViewById<TextView>(Resource.Id.textViewSpan).Text = $"Spanned: {isSpanned}";
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
