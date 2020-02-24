d0af1cbe4fe75c37e1322299a7e7d4e835767fa8
Jonathan Dick <jondick@gmail.com>
2020-02-24 13:33:11 -0500

    Make sample show hinge coordinates

--- Android/SurfaceDuo/samples/DuoSample/MainActivity.cs
+++ Android/SurfaceDuo/samples/DuoSample/MainActivity.cs
@@ -4,35 +4,51 @@ using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
 using Microsoft.Device.Display;
+using Android.Content.Res;
 
 namespace DuoSample
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true,
+        ConfigurationChanges = Android.Content.PM.ConfigChanges.ScreenSize | Android.Content.PM.ConfigChanges.ScreenLayout | Android.Content.PM.ConfigChanges.Orientation | Android.Content.PM.ConfigChanges.SmallestScreenSize)]
     public class MainActivity : AppCompatActivity
     {
         ScreenHelper screenHelper;
+        bool isDuo;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
 
             screenHelper = new ScreenHelper();
-            var isDuo = screenHelper.Initialize(this);
+            isDuo = screenHelper.Initialize(this);
 
+            Update();
+        }
+
+        public override void OnConfigurationChanged(Configuration newConfig)
+        {
+            base.OnConfigurationChanged(newConfig);
+
+            screenHelper.OnConfigurationChanged(newConfig);
+
+            Update();
+        }
+
+        void Update()
+        {
             var isSpanned = screenHelper.IsDualMode;
+
+            var hb = screenHelper.GetHingeBounds();
+            var hbd = screenHelper.GetHingeBoundsDip();
+
+            var hingeText = $"Hinge:    x:{hb.Left}, y:{hb.Top}, w:{hb.Width()}, h:{hb.Height()}";
+            hingeText += $"\r\nHinge DP: x:{hbd.Left}, y:{hbd.Top}, w:{hbd.Width()}, h:{hbd.Height()}";
 
             FindViewById<TextView>(Resource.Id.textViewDuo).Text = $"Duo: {isDuo}";
             FindViewById<TextView>(Resource.Id.textViewSpan).Text = $"Spanned: {isSpanned}";
-        }
-
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            FindViewById<TextView>(Resource.Id.textViewHinge).Text = hingeText;
         }
     }
 }
