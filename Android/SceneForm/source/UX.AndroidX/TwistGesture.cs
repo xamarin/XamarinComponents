73e85f73ee4fa4246303477a4a66c54eb79d4304
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2020-03-05 14:10:03 +0100

    Rendering fixes, UX.ANdroidX added, switching to windows

---
+++ Android/SceneForm/source/UX.AndroidX/TwistGesture.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Google.AR.Sceneform.UX
+{
+    public partial class TwistGesture
+    {
+        protected override Java.Lang.Object RawSelf => this;
+    }
+}
