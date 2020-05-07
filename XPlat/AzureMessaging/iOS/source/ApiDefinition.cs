5962534fce92bc8037f1920b37a5c5cee04085b5
Olga Chistyakova <v-olchi@microsoft.com>
2020-05-07 16:36:51 +0300

    Added nh sdk-v3 bindings.

--- XPlat/AzureMessaging/iOS/source/ApiDefinition.cs
+++ XPlat/AzureMessaging/iOS/source/ApiDefinition.cs
@@ -8,6 +8,157 @@ namespace WindowsAzure.Messaging
 	public delegate void ErrorCallback(NSError error);
 
 	[BaseType (typeof (NSObject))]
+	public partial interface MSNotificationHub 
+	{
+		[Static, Export ("initWithConnectionString:hubName:")]
+		void Init (string connectionString, string hubName);
+
+		[Static, Export ("setDelegate:")]
+		void SetDelegate ([NullAllowed] id<MSNotificationHubDelegate> delegate);
+
+		[Static, Export ("setTemplate:forKey:")]
+		void SetTemplate (MSInstallationTemplate template, string key);
+
+		[Static, Export ("removeTemplate:")]
+		void RemoveTemplate (string key);
+
+		[Static, Export ("getTemplate:")]
+		MSInstallationTemplate GetTemplate (string key);
+		
+		[Static, Export ("addTag:")]
+		bool AddTag (string tag);
+
+		[Static, Export ("addTags:")]
+		bool AddTags (NSArray<string> tags);
+
+		[Static, Export ("removeTag:")]
+		bool RemoveTag (string tag);
+
+		[Static, Export ("removeTags:")]
+		bool RemoveTags (NSArray<string> tags);
+
+		[Static, Export ("getTags")]
+		NSArray<string> GetTags ();
+
+		[Static, Export ("clearTags")]
+		void ClearTags ();
+
+		[Static, Export ("getInstallation")]
+		MSInstallation GetInstallation ();
+
+		[Static, Export ("didRegisterForRemoteNotificationsWithDeviceToken:")]
+		void DidRegisterForRemoteNotifications (NSData deviceToken);
+
+		[Static, Export ("didFailToRegisterForRemoteNotificationsWithError:")]
+		void DidFailToRegisterForRemoteNotifications (NSError error);
+
+		[Static, Export ("didReceiveRemoteNotification:")]
+		bool DidReceiveRemoteNotification (NSDictionary userInfo);
+	}
+
+	[BaseType (typeof (NSObject))]
+	public partial interface MSInstallation 
+	{
+		[Export ("installationID")]
+		string InstallationID { get;set; }
+
+		[Export ("pushChannel")]
+		string PushChannel { get;set; }
+
+		[Export ("platform")]
+		string Platform { get;set; }
+
+		[Export ("tags")]
+		NSArray<string> Tags { get;set; }
+	}
+
+	[BaseType (typeof (NSObject))]
+	public partial interface MSInstallationTemplate 
+	{
+	}
+
+	[BaseType(typeof(NSObject))]
+    [Model, Protocol]
+    public interface MSNotificationHubDelegate 
+	{
+        [Export ("notificationHub:didReceivePushNotification:")]
+    	void DidReceivePushNotification(MSNotificationHub notificationHub, MSNotificationHubMessage message);
+	}
+
+	[BaseType (typeof (NSObject))]
+	public partial interface MSNotificationHubMessage 
+	{
+		[Export ("title")]
+		string Title { get; }
+
+		[Export ("message")]
+		string Message { get; }
+
+		[Export ("badge")]
+		int Badge { get; }
+
+		[Export ("additionalData")]
+		NSMutableDictionary<NSString, NSString>  AdditionalData { get; }
+	}
+
+	[BaseType (typeof (NSObject))]
+	public partial interface SBRegistration
+	{
+		[Export ("ETag")]
+		string ETag { get;set; }
+
+		[Export ("expiresAt")]
+		NSDate ExpiresAt { get;set; }
+
+		[Export ("tags")]
+		NSSet Tags { get;set; }
+
+		[Export ("registrationId")]
+		string RegistrationId { get;set; }
+
+		[Export ("deviceToken")]
+		string DeviceToken { get;set; }
+
+		[Static, Export ("Name")]
+		string Name ();
+
+		[Static, Export ("payloadWithDeviceToken:tags:")]
+		string Payload (string deviceToken, NSSet tags);
+	}
+
+	[BaseType (typeof (NSObject))]
+	public partial interface StoredRegistrationEntry
+	{
+		[Export ("RegistrationName")]
+		string RegistrationName { get;set; }
+
+		[Export ("RegistrationId")]
+		string RegistrationId { get;set; }
+
+		[Export ("ETag")]
+		string ETag { get;set; }
+
+		[Export ("initWithString:")]
+		IntPtr Constructor (string str);
+
+		[Export ("toString")]
+		string AsString ();
+	}
+
+	[BaseType (typeof (NSObject))]
+	public partial interface SBTokenProvider
+	{
+		[Export ("timeToExpireinMins")]
+		int TimeToExpireInMin { get;set; }
+
+		[Export ("setTokenWithRequest:completion:")]
+		void SetToken (NSMutableUrlRequest request, ErrorCallback errorCallback);
+
+		[Export ("setTokenWithRequest:error:")]
+		bool SetToken (NSMutableUrlRequest request, out NSError error);
+	}
+
+	[BaseType (typeof (NSObject))]
 	public partial interface SBConnectionString 
 	{
 		[Static, Export ("stringWithEndpoint:issuer:issuerSecret:")]
