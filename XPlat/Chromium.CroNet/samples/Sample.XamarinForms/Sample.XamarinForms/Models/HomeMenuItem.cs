6dcdad24e7f48a3b9860fb595365da1ddb124537
moljac <mcvjetko@holisticware.net>
2020-05-14 13:03:24 +0200

    forms sample added

---
+++ XPlat/Chromium.CroNet/samples/Sample.XamarinForms/Sample.XamarinForms/Models/HomeMenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sample.XamarinForms.Models
+{
+    public enum MenuItemType
+    {
+        Browse,
+        About
+    }
+    public class HomeMenuItem
+    {
+        public MenuItemType Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
