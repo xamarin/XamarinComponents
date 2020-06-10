6d190a79ed5003fec26d45601399a2f6f78b48f1
Matthew Leibowitz <mattleibow@live.com>
2020-06-10 15:32:58 +0200

    Imported Xamarin.ExposureNotification source (#895)

---
+++ XPlat/ExposureNotification/source/Xamarin.ExposureNotification/ExposureNotification.netstandard.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Xamarin.ExposureNotifications
+{
+	public static partial class ExposureNotification
+	{
+		static void PlatformInit()
+			=> throw new PlatformNotSupportedException();
+
+		static Task PlatformStart()
+			=> throw new PlatformNotSupportedException();
+
+		static Task PlatformStop()
+			=> throw new PlatformNotSupportedException();
+
+		static Task<bool> PlatformIsEnabled()
+			=> throw new PlatformNotSupportedException();
+
+		static Task PlatformScheduleFetch()
+			=> throw new PlatformNotSupportedException();
+
+		static Task<(ExposureDetectionSummary, IEnumerable<ExposureInfo>)> PlatformDetectExposuresAsync(IEnumerable<TemporaryExposureKey> diagnosisKeys, CancellationToken cancellationToken)
+			=> throw new PlatformNotSupportedException();
+
+		static Task<IEnumerable<TemporaryExposureKey>> PlatformGetTemporaryExposureKeys()
+			=> throw new PlatformNotSupportedException();
+
+		static Task<Status> PlatformGetStatusAsync()
+			=> throw new PlatformNotSupportedException();
+	}
+}
