0b8ad458a9ff73737e4306a22044068deadac8ae
Kazumi Hirose <kazumihirose@hotmail.com>
2020-06-28 21:23:42 +0900

    Fix Android API Rolling Period Bug , Enable Xamarin EN side.
    Change
    TimeSpan.Zero, // TODO: TimeSpan.FromMinutes(k.RollingDuration * 10),

--- XPlat/ExposureNotification/source/Xamarin.ExposureNotification/ExposureNotification.android.cs
+++ XPlat/ExposureNotification/source/Xamarin.ExposureNotification/ExposureNotification.android.cs
@@ -171,7 +171,7 @@ namespace Xamarin.ExposureNotifications
 						new TemporaryExposureKey(
 							k.GetKeyData(),
 							k.RollingStartIntervalNumber,
-							TimeSpan.Zero, // TODO: TimeSpan.FromMinutes(k.RollingDuration * 10),
+							TimeSpan.FromMinutes(k.RollingDuration * 10),
 							k.TransmissionRiskLevel.FromNative()));
 				});
 
