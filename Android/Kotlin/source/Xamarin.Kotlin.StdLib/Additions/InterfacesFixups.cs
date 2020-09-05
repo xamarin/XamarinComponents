3f91456f962b28640a9f15589ec856a0f425e5d5
Antonio Seric <antonio@lilcodelab.com>
2020-09-06 01:17:47 +0200

    Added kotlin 1.4

--- Android/Kotlin/source/Xamarin.Kotlin.StdLib/Additions/InterfacesFixups.cs
+++ Android/Kotlin/source/Xamarin.Kotlin.StdLib/Additions/InterfacesFixups.cs
@@ -10,4 +10,38 @@ namespace Kotlin.Collections
 	partial class AbstractListInvoker : Java.Util.IList
 	{
 	}
+
+    public abstract partial class ULongIterator
+    {
+        unsafe global::Java.Lang.Object global::Java.Util.IIterator.Next()
+        {
+            return (Java.Lang.Long)(long)Next();
+        }
+
+    }
+
+    public abstract partial class UByteIterator 
+    {
+        unsafe global::Java.Lang.Object global::Java.Util.IIterator.Next()
+        {
+            return (Java.Lang.Byte)(sbyte)Next();
+        }
+
+    }
+
+    public abstract partial class UIntIterator
+    {
+        unsafe global::Java.Lang.Object global::Java.Util.IIterator.Next()
+        {
+            return (Java.Lang.Integer)(int)Next();
+        }
+    }
+
+    public abstract partial class UShortIterator
+    {
+        unsafe global::Java.Lang.Object global::Java.Util.IIterator.Next()
+        {
+            return (Java.Lang.Short)(short)Next();
+        }
+    }
 }
---
+++ Android/Kotlin/source/Xamarin.Kotlin.StdLib/Additions/RangesFix.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Kotlin.Ranges
+{
+	public sealed partial class ULongRange
+    {
+        Java.Lang.Long Start => (Java.Lang.Long)(long)GetStart();
+
+        Java.Lang.Long EndInclusive => (Java.Lang.Long)(long)GetEndInclusive();
+    }
+
+    public sealed partial class UIntRange
+    {
+        Java.Lang.Integer Start => (Java.Lang.Integer)(int)GetStart();
+
+        Java.Lang.Integer EndInclusive => (Java.Lang.Integer)(int)GetEndInclusive();
+    }
+}
