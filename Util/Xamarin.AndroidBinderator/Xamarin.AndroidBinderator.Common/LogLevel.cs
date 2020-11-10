77043caacf1827e3499f786d89ef5cf43c9e2269
Christiano Donke <cdonke@outlook.com>
2020-11-10 12:34:46 -0300

    Logging

---
+++ Util/Xamarin.AndroidBinderator/Xamarin.AndroidBinderator.Common/LogLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AndroidBinderator.Common
+{
+    public enum LogLevel
+    {
+        Debug = 1,
+        Information = 2,
+        Warning = 3,
+        Error = 4,
+        Critical = 5,
+    }
+}
