e51abd943d129c59a676def2da63735c5693fd90
Antonio Seric <antonio@lilcodelab.com>
2020-12-19 01:11:48 +0100

    fix sample app dependency, fix native app warning

--- Android/Kotlin/native/KotlinSample/kotlinsamplelibrary/src/main/java/com/example/kotlinsamplelibrary/TestClass.kt
+++ Android/Kotlin/native/KotlinSample/kotlinsamplelibrary/src/main/java/com/example/kotlinsamplelibrary/TestClass.kt
@@ -3,35 +3,38 @@ package com.example.kotlinsamplelibrary
 import java.util.ArrayList
 
 class TestClass {
+
+    companion object {
+        private val isEven = IntPredicate { it % 2 == 0 }
+    }
+
     var testProperty: String = "Not an empty string!"
 
-    fun Test(array: Array<String>): List<String> {
+    fun test(array: Array<String>): List<String> {
         val list = ArrayList<String>()
         array.forEach { str -> list.add(str) }
         return list
     }
 
-    fun TestUnsignedArray(): ULongArray {
+    fun testUnsignedArray(): ULongArray {
         return ulongArrayOf(1u, 2u, 3u, 4u, 5u, 6u, 7u, 8u, 9u, 10u, 18446744073709551614u, 18446744073709551615u)
     }
 
-    fun TestUnsigned(): UInt {
+    fun testUnsigned(): UInt {
         var i: UInt = 4294967294u
-        i = i + 1u;
-        return i;
+        i = i + 1u
+        return i
     }
 
-    fun TestIsEven(i: Int): String {
-        return "Is ${i} even? - ${isEven.accept(i)}"
+    fun testIsEven(i: Int): String {
+        return "Is $i even? - ${isEven.accept(i)}"
     }
 
-    fun interface IntPredicate {
+    private fun interface IntPredicate {
         fun accept(i: Int): Boolean
     }
 
-    private val isEven = IntPredicate { it % 2 == 0 }
-
-    fun TestBitOperations() : String {
+    fun testBitOperations() : String {
         val number = "1010000".toUInt(radix = 2)
         return "countOneBits:${number.countOneBits()},\n" +
                "countTrailingZeroBits:${number.countTrailingZeroBits()},\n" +
