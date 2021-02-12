b1c797355203dc360d935a2d2f898734faceb64a
Jonathan Pryor <jonpryor@vt.edu>
2021-02-12 18:39:49 -0500

    [Shopify] Remove credentials from sample code.

--- XPlat/Shopify/iOS/samples/ShopifyiOSSample/AppDelegate.cs
+++ XPlat/Shopify/iOS/samples/ShopifyiOSSample/AppDelegate.cs
@@ -31,12 +31,12 @@ namespace ShopifyiOSSample
 	public class AppDelegate : UIApplicationDelegate
 	{
 		// Enter your shop domain and API Key
-		public const string SHOP_DOMAIN = "xamarinbindingexample.myshopify.com";
-		public const string API_KEY = "6149b5dbe23ad2c34bb7098481471e33";
-		public const string CHANNEL_ID = "46682499";
+		public const string SHOP_DOMAIN = "TODO-xamarinbindingexample.myshopify.com";
+		public const string API_KEY = "TODO-API-KEY";
+		public const string CHANNEL_ID = "TODO-CHANNEL-ID";
 
 		// Enter your merchant ID
-		public const string MERCHANT_ID = "merchant.com.xamarintests.xamarinbindings";
+		public const string MERCHANT_ID = "TODO-MERCHANT-ID";
 
 		public static NSString CheckoutCallbackNotification = (NSString)"CheckoutCallbackNotification";
 
