ffa5607a1916aa3f092556983e2014fb8908ca80
Benjamin Mayrargue <benjamin@vapolia.fr>
2022-01-17 19:40:44 +0100

    Update arcore to 1.29 Fix issue when building with cake, where changing the arcore version won't override the already downloaded libraries.

--- Android/ARCore/source/Additions/Additions.cs
+++ Android/ARCore/source/Additions/Additions.cs
@@ -1,1 +1,13 @@
-﻿
+﻿//using System;
+
+//namespace Google.AR.Core
+//{
+
+//    public static class ArCoreExtensions
+//    {
+//        public static void Test()
+//        {
+//            new Frame(IntPtr.Zero, null).AcquireRawDepthImage
+//        }
+//    }
+//}
