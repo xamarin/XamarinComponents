02606c64e4952fdbcbff5183bf698db58ed9f320
Jonathan Pobst <jonathan.pobst@microsoft.com>
2022-11-29 12:45:29 -0600

    Remove packages that are now maintained in other repositories. (#1417)

--- Android/SquareOkHttp3/source/Square.OkHttp3/Dispatcher.cs
+++
@@ -1,8 +0,0 @@
-namespace Square.OkHttp3
-{
-    partial class Dispatcher
-    {
-        public void SetIdleCallback(Java.Lang.IRunnable idleCallback) =>
-            IdleCallback = idleCallback;
-    }
-}
